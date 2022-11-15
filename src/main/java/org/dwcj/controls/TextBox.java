e027b6d6a5a2b9ac12c6cb50965efc22d7b87b0c
Matthew Hawkins <mhawkins@basis.cloud>
2022-11-15 11:12:52 +0100

    Completed interface, member variable and catchup implementation for textbox

--- src/main/java/org/dwcj/controls/TextBox.java
+++ src/main/java/org/dwcj/controls/TextBox.java
@@ -3,12 +3,11 @@ package org.dwcj.controls;
 import com.basis.bbj.proxies.sysgui.BBjEditBox;
 import com.basis.bbj.proxies.sysgui.BBjWindow;
 import com.basis.startup.type.BBjException;
-
 import org.dwcj.bridge.PanelAccessor;
 import org.dwcj.panels.AbstractDwcjPanel;
 
 
-public final class TextBox extends AbstractDwcControl implements IReadOnly{
+public final class TextBox extends AbstractDwcControl implements IReadOnly, IFocusable, ITabTraversable, ITextAlignable, ITextControl {
 
     private BBjEditBox bbjEditBox;
 
@@ -22,7 +21,14 @@ public final class TextBox extends AbstractDwcControl implements IReadOnly{
     }
 
 
+    private Integer maxLength = 2147483647;
+    private Boolean homeDelete = false;
+    private Boolean passwordVisible = false;
+
+
+
     public TextBox() {
+        this("");
     }
 
     public TextBox(String text) {
@@ -32,8 +38,6 @@ public final class TextBox extends AbstractDwcControl implements IReadOnly{
     
     @Override
     protected void create(AbstractDwcjPanel p) {
-        
-        
         try {
             BBjWindow w = PanelAccessor.getDefault().getBBjWindow(p);
             //todo: honor visibility flag, if set before adding the control to the form, so it's created invisibly right away
@@ -47,37 +51,53 @@ public final class TextBox extends AbstractDwcControl implements IReadOnly{
     }
 
     public String getEditType(){
-        return bbjEditBox.getEditType();
-    }
-
-    public int getMaxLength(){
-        return bbjEditBox.getMaxLength();
-    }
-
-    public boolean isPassHomeDelete(){
-        try{
-            return bbjEditBox.getPassHomeDelete();
-        } catch(BBjException e){
-            e.printStackTrace();
+        if(this.ctrl != null){
+            return bbjEditBox.getEditType();
         }
-        return false;
+        return "";
+    }
+
+    public Integer getMaxLength(){
+        if(this.ctrl != null){
+            return bbjEditBox.getMaxLength();
+        }
+        return this.maxLength;
+    }
+
+    public Boolean isPassHomeDelete(){
+        if(this.ctrl != null){
+            try{
+                return bbjEditBox.getPassHomeDelete();
+            } catch(BBjException e){
+                e.printStackTrace();
+            }
+        }
+        return this.homeDelete;
     }
 
     public String getSelectedText(){
-        try{
-            return bbjEditBox.getSelectedText();
-        } catch(BBjException e){
-            e.printStackTrace();
+        if(this.ctrl != null){
+            try{
+                return bbjEditBox.getSelectedText();
+            } catch(BBjException e){
+                e.printStackTrace();
+            }
         }
         return null;
     }
 
     /*Unsure if this is the correct return type for this functionality -MH */
-    public Object[] getSelection(){
-        try{
-            return bbjEditBox.getSelection().toArray();
-        } catch(BBjException e){
-            e.printStackTrace();
+
+    /*Changed this to return a single string, otherwise could not get this to
+     * properly work -MH
+     */
+    public String getSelection(){
+        if(this.ctrl != null){
+            try{
+                return bbjEditBox.getSelection().toArray().toString();
+            } catch(BBjException e){
+                e.printStackTrace();
+            }
         }
         return null;
     }
@@ -85,49 +105,61 @@ public final class TextBox extends AbstractDwcControl implements IReadOnly{
     
     
     public boolean isPasswordVisible(){
+        if(this.ctrl != null)
         try{
             return bbjEditBox.isPasswordVisible();
         }
         catch(BBjException e){
             e.printStackTrace();
         }
-        return false;
+        return this.passwordVisible;
     }
 
-    public TextBox select(int offset1, int offset2){
-        bbjEditBox.select(offset1, offset2);
+    public TextBox select(Integer offset1, Integer offset2){
+        if(this.ctrl != null){
+            bbjEditBox.select(offset1, offset2);
+        }
         return this;    
     }
 
     
 
-    public TextBox setMaxLength(int length){
-        try{
-            bbjEditBox.setMaxLength(length);
+    public TextBox setMaxLength(Integer length){
+        if(this.ctrl != null){
+            try{
+                bbjEditBox.setMaxLength(length);
+            }
+            catch(BBjException e){
+                e.printStackTrace();
+            }
         }
-        catch(BBjException e){
-            e.printStackTrace();
-        }
+        this.maxLength = length;
         return this;
     }
 
-    public TextBox setPassHomeDelete(boolean pass){
-        try{
-            bbjEditBox.setPassHomeDelete(pass);
+    public TextBox setPassHomeDelete(Boolean pass){
+        if(this.ctrl != null){
+            try{
+                bbjEditBox.setPassHomeDelete(pass);
+            }
+            catch(BBjException e){
+                e.printStackTrace();
+            }
         }
-        catch(BBjException e){
-            e.printStackTrace();
-        }
+        this.homeDelete = pass;
         return this;
     }
 
-    public TextBox setPasswordVisible(boolean visible){
-        try{
-            bbjEditBox.setPasswordVisible(visible);
+    public TextBox setPasswordVisible(Boolean visible){
+        if(this.ctrl != null){
+            try{
+                bbjEditBox.setPasswordVisible(visible);
+            }
+            catch(BBjException e){
+                e.printStackTrace();
+            }
         }
-        catch(BBjException e){
-            e.printStackTrace();
-        }
+        this.passwordVisible = visible;
         return this;
     }
 
@@ -156,6 +188,95 @@ public final class TextBox extends AbstractDwcControl implements IReadOnly{
         }
         return this;
     }
+
+    @Override
+    public Boolean isFocusable(){
+        if(this.ctrl != null){
+            try{
+                bbjEditBox.isFocusable();
+            } catch(BBjException e){
+                e.printStackTrace();
+            }
+        }
+        return this.focusable;
+    }
+
+    @Override 
+    public TextBox setFocusable(Boolean focusable){
+        if(this.ctrl != null){
+            try{
+                bbjEditBox.setFocusable(focusable);
+            } catch(BBjException e){
+                e.printStackTrace();
+            }
+        }
+        this.focusable = focusable;
+        return this;
+    }
+
+    @Override
+    public Boolean isTabTraversable(){
+        if(this.ctrl != null){
+            try{
+                bbjEditBox.isTabTraversable();
+            } catch(BBjException e){
+                e.printStackTrace();
+            }
+        }
+        return this.tabTraversable;
+    }
+
+    @Override
+    public TextBox setTabTraversable(Boolean traversable){
+        if(this.ctrl != null){
+            try{
+                bbjEditBox.setTabTraversable(traversable);
+            } catch(BBjException e){
+                e.printStackTrace();
+            }
+        }
+        this.tabTraversable = traversable;
+        return this;
+    }
+
+    @Override
+    public Alignment getTextAlignment(){
+        return this.textAlignment;
+    }
+
+    @Override
+    public TextBox setTextAlignment(Alignment alignment) {
+        //todo: why could an exception be thrown?
+        if(this.ctrl != null){
+            try {
+                bbjEditBox.setAlignment(alignment.textPosition);
+            } catch (BBjException e) {
+                e.printStackTrace();
+            }
+        }
+        this.textAlignment = alignment;
+        return this;
+    }
+
+
+    @Override
+    public Highlight getHighlightOnFocus(){
+        return this.textHighlight;
+    } 
+
+    @Override
+    public TextBox setHighlightOnFocus(Highlight highlight){
+        if(this.ctrl != null){
+            try{
+                bbjEditBox.setHighlightOnFocus(highlight.highlight);
+            } catch (BBjException e){
+                e.printStackTrace();
+            }
+        }
+        this.textHighlight = highlight;
+        return this;
+    }
+
 
 
 
@@ -225,6 +346,47 @@ public final class TextBox extends AbstractDwcControl implements IReadOnly{
         super.setControlTheme(theme);
         return this;
     }
+
+
+    @Override
+    protected void catchUp() throws IllegalAccessException {
+        super.catchUp();
+
+                
+        if(this.maxLength != 2147483647){
+            this.setMaxLength(this.maxLength);
+        }
+
+        if(this.homeDelete != false){
+            this.setPassHomeDelete(this.homeDelete);
+        }
+        
+        if(this.passwordVisible != false){
+            this.setPasswordVisible(this.passwordVisible);
+        }
+        
+
+        if(this.readOnly != false){
+            this.setReadOnly(this.readOnly);
+        }
+
+        if(this.focusable != true){
+            this.setFocusable(this.focusable);
+        }
+
+        if(this.tabTraversable != true){
+            this.setTabTraversable(this.tabTraversable);
+        }
+
+        if(this.textAlignment != Alignment.LEFT){
+            this.setTextAlignment(this.textAlignment);
+        }
+
+        if(this.textHighlight != Highlight.HIGHLIGHT_NONE){
+            this.setHighlightOnFocus(this.textHighlight);
+        }
+    }
+
 
 
 }
