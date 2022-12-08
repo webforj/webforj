a903c338662a7c0d85629e719a501a5d94dc2b2b
Matthew Hawkins <mhawkins@basis.cloud>
2022-12-08 16:49:35 +0100

    Removed extended demos which were accidentally included after editing

--- src/main/java/org/dwcj/extendeddemos/bbjclasses/BBjClassDemoIF.java
+++
@@ -1,9 +0,0 @@
-package org.dwcj.extendeddemos.bbjclasses;
-
-import java.math.BigDecimal;
-
-public interface BBjClassDemoIF {
-
-    public BigDecimal multiply(BigDecimal factor1, BigDecimal factor2);
-
-}
