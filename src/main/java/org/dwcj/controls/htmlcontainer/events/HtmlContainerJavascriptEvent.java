3413b1cf4fcba4426192d71e7c375c54d9defe70
Stephan Wald <swald@basis.com>
2022-12-27 12:55:42 +0100

    add EventMap

--- src/main/java/org/dwcj/controls/htmlcontainer/events/HtmlContainerJavascriptEvent.java
+++ src/main/java/org/dwcj/controls/htmlcontainer/events/HtmlContainerJavascriptEvent.java
@@ -3,16 +3,25 @@ package org.dwcj.controls.htmlcontainer.events;
 import org.dwcj.controls.htmlcontainer.HtmlContainer;
 import org.dwcj.interfaces.ControlEvent;
 
+import java.util.Map;
+
 public final class HtmlContainerJavascriptEvent implements ControlEvent {
 
     private final HtmlContainer control;
 
-    public HtmlContainerJavascriptEvent(HtmlContainer h) {
+    private final Map<Object,Object> eventMap;
+
+    public HtmlContainerJavascriptEvent(HtmlContainer h, Map eventMap) { //NOSONAR
         this.control = h;
+        this.eventMap = eventMap;
     }
 
     @Override
     public HtmlContainer getControl() {
         return control;
+    }
+
+    public Map<Object,Object> getEventMap() {
+        return eventMap;
     }
 }
--- src/main/java/org/dwcj/controls/htmlcontainer/sinks/HtmlContainerNativeJavascriptEventSink.java
+++ src/main/java/org/dwcj/controls/htmlcontainer/sinks/HtmlContainerNativeJavascriptEventSink.java
@@ -2,6 +2,8 @@ package org.dwcj.controls.htmlcontainer.sinks;
 
 import com.basis.bbj.proxies.event.BBjNativeJavaScriptEvent;
 import com.basis.bbj.proxies.sysgui.BBjControl;
+import com.basis.startup.type.BBjException;
+import org.dwcj.App;
 import org.dwcj.Environment;
 import org.dwcj.bridge.ControlAccessor;
 import org.dwcj.controls.htmlcontainer.HtmlContainer;
@@ -15,12 +17,11 @@ public final class HtmlContainerNativeJavascriptEventSink {
     private final Consumer<HtmlContainerJavascriptEvent> target;
     private final HtmlContainer container;
 
-    @SuppressWarnings({"static-access"})
     public HtmlContainerNativeJavascriptEventSink(HtmlContainer htmlv, Consumer<HtmlContainerJavascriptEvent> target) {
         this.target = target;
         this.container = htmlv;
 
-        BBjControl bbjctrl = null;
+        BBjControl bbjctrl;
         try {
             bbjctrl = ControlAccessor.getDefault().getBBjControl(htmlv);
             bbjctrl.setCallback(Environment.getInstance().getBBjAPI().ON_NATIVE_JAVASCRIPT, Environment.getInstance().getDwcjHelper().getEventProxy(this, "onEvent"), "onEvent");
@@ -30,7 +31,13 @@ public final class HtmlContainerNativeJavascriptEventSink {
     }
 
     public void onEvent(BBjNativeJavaScriptEvent ev) { //NOSONAR
-        HtmlContainerJavascriptEvent dwcEv = new HtmlContainerJavascriptEvent(container);
+        HtmlContainerJavascriptEvent dwcEv;
+        try {
+            dwcEv = new HtmlContainerJavascriptEvent(container, ev.getEventMap());
+        } catch (BBjException e) {
+            dwcEv = new HtmlContainerJavascriptEvent(container, null);
+            App.consoleError("Error: could not determine JS event map!");
+        }
         target.accept(dwcEv);
     }
 }
