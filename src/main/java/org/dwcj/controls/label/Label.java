44d0252ad29071862a9b853fa110ea1f8670d2cd
Matthew Hawkins <mhawkins@basis.cloud>
2023-01-19 09:47:39 +0100

    Removed line wrap, as this did not work in DWCJ. Also commented out the set/get margin functions, as this introduces a redundant and potentially confusing way to change the padding of an element which should be done with CSS instead.

--- src/main/java/org/dwcj/controls/label/Label.java
+++ src/main/java/org/dwcj/controls/label/Label.java
@@ -15,18 +15,21 @@ public final class Label extends AbstractDwcControl implements TextAlignable {
 
     private BBjStaticText bbjStaticText;
 
-    private Boolean lineWrap = true;
 
     /* top, right, bottom, left margins,
      * used simple array as user will not 
      * have to interact directly with this
      */
-    private Integer[] margins = {0,0,0,0};
+    // private Integer[] margins = {0,0,0,0};
 
     public Label() {
         this("");
     }
 
+    /**
+     * Constructor used to give the label initial text
+     * @param text String value for initial display text
+     */
     public Label(String text) {
         setText(text);
         this.textAlignment = Alignment.LEFT;
@@ -45,120 +48,101 @@ public final class Label extends AbstractDwcControl implements TextAlignable {
         }
     }
 
-    public int getBottomMargin(){
-        if(this.ctrl != null){
-            try{
-                return bbjStaticText.getBottomMargin();
-            } catch (BBjException e){
-                Environment.logError(e);
-            }
-        }
-        return this.margins[2];
-    }
+    /**
+     * Returns the value of the bottom margin added below the text
+     * @return Integer value 
+     */
+    // public Integer getBottomMargin(){
+    //     if(this.ctrl != null){
+    //         try{
+    //             return bbjStaticText.getBottomMargin();
+    //         } catch (BBjException e){
+    //             Environment.logError(e);
+    //         }
+    //     }
+    //     return this.margins[2];
+    // }
 
-    public int getLeftMargin(){
-        if(this.ctrl != null){
-            try{
-                return bbjStaticText.getLeftMargin();
-            } catch (BBjException e){
-                Environment.logError(e);
-            }
-        }
-        return this.margins[3];
-    }
+    // public Integer getLeftMargin(){
+    //     if(this.ctrl != null){
+    //         try{
+    //             return bbjStaticText.getLeftMargin();
+    //         } catch (BBjException e){
+    //             Environment.logError(e);
+    //         }
+    //     }
+    //     return this.margins[3];
+    // }
 
-    public boolean isLineWrap(){
-        if(this.ctrl != null){
-            try{
-                return bbjStaticText.getLineWrap();
-            } catch (BBjException e){
-                Environment.logError(e);
-            }
-        }
-        return this.lineWrap;
-    }
+    // public Integer getRightMargin(){
+    //     if(this.ctrl != null){
+    //         try{
+    //             return bbjStaticText.getRightMargin();
+    //         } catch (BBjException e){
+    //             Environment.logError(e);
+    //         }
+    //     }
+    //     return this.margins[1];
+    // }
 
-    public int getRightMargin(){
-        if(this.ctrl != null){
-            try{
-                return bbjStaticText.getRightMargin();
-            } catch (BBjException e){
-                Environment.logError(e);
-            }
-        }
-        return this.margins[1];
-    }
+    // public Integer getTopMargin(){
+    //     if(this.ctrl != null){
+    //         try{
+    //             return bbjStaticText.getTopMargin();
+    //         } catch (BBjException e){
+    //             Environment.logError(e);
+    //         }
+    //     }
+    //     return this.margins[0];
+    // }
 
-    public int getTopMargin(){
-        if(this.ctrl != null){
-            try{
-                return bbjStaticText.getTopMargin();
-            } catch (BBjException e){
-                Environment.logError(e);
-            }
-        }
-        return this.margins[0];
-    }
-
-    public Label setBottomMargin(int margin){
-        if(this.ctrl != null){
-            try{
-                bbjStaticText.setBottomMargin(margin);
-            } catch (BBjException e){
-                Environment.logError(e);
-            }
-        }
-        this.margins[2] = margin;
-        return this;
-    }
+    // public Label setBottomMargin(int margin){
+    //     if(this.ctrl != null){
+    //         try{
+    //             bbjStaticText.setBottomMargin(margin);
+    //         } catch (BBjException e){
+    //             Environment.logError(e);
+    //         }
+    //     }
+    //     this.margins[2] = margin;
+    //     return this;
+    // }
     
-    public Label setLeftMargin(int margin){
-        if(this.ctrl != null){
-            try{
-                bbjStaticText.setLeftMargin(margin);
-            } catch (BBjException e){
-                Environment.logError(e);
-            }
-        }
-        this.margins[3] = margin;
-        return this;
-    }
+    // public Label setLeftMargin(int margin){
+    //     if(this.ctrl != null){
+    //         try{
+    //             bbjStaticText.setLeftMargin(margin);
+    //         } catch (BBjException e){
+    //             Environment.logError(e);
+    //         }
+    //     }
+    //     this.margins[3] = margin;
+    //     return this;
+    // }
 
-    public Label setLineWrap(boolean wrap){
-        if(this.ctrl != null){
-            try{
-                bbjStaticText.setLineWrap(wrap);
-            } catch (BBjException e){
-                Environment.logError(e);
-            }
-        }
-        this.lineWrap = wrap;
-        return this;
-    }
+    // public Label setRightMargin(int margin){
+    //     if(this.ctrl != null){
+    //         try{
+    //             bbjStaticText.setRightMargin(margin);
+    //         } catch (BBjException e){
+    //             Environment.logError(e);
+    //         }
+    //     }
+    //     this.margins[1] = margin;
+    //     return this;
+    // }
 
-    public Label setRightMargin(int margin){
-        if(this.ctrl != null){
-            try{
-                bbjStaticText.setRightMargin(margin);
-            } catch (BBjException e){
-                Environment.logError(e);
-            }
-        }
-        this.margins[1] = margin;
-        return this;
-    }
-
-    public Label setTopMargin(int margin){
-        if(this.ctrl != null){
-            try{
-                bbjStaticText.setTopMargin(margin);
-            } catch (BBjException e){
-                Environment.logError(e);
-            }
-        }
-        this.margins[0] = margin;
-        return this;
-    }
+    // public Label setTopMargin(int margin){
+    //     if(this.ctrl != null){
+    //         try{
+    //             bbjStaticText.setTopMargin(margin);
+    //         } catch (BBjException e){
+    //             Environment.logError(e);
+    //         }
+    //     }
+    //     this.margins[0] = margin;
+    //     return this;
+    // }
 
 
 
@@ -241,27 +225,7 @@ public final class Label extends AbstractDwcControl implements TextAlignable {
     @SuppressWarnings("java:S3776") // tolerate cognitive complexity for now, it's just a batch list of checks
     protected void catchUp() throws IllegalAccessException {
         if (Boolean.TRUE.equals(this.getCaughtUp())) throw new IllegalAccessException("catchUp cannot be called twice");
-        
         super.catchUp();
-
-        if(Boolean.TRUE.equals(this.lineWrap)){
-            this.setLineWrap(this.lineWrap);
-        }
-
-
-        if(this.margins[0] != 0){
-            this.setTopMargin(this.margins[0]);
-        }
-        if(this.margins[1] != 0){
-            this.setRightMargin(this.margins[1]);
-        }
-        if(this.margins[2] != 0){
-            this.setBottomMargin(this.margins[2]);
-        }
-        if(this.margins[3] != 0){
-            this.setLeftMargin(this.margins[3]);
-        }
-
     }
 
 
