775a44095a047e4e8fccbb7aacb9e87f9546026a
Hyyan Abo Fakher <hyyanaf@gmail.com>
2023-03-08 11:05:00 +0100

    fix: TabCtrl::add throws `java.util.ConcurrentModificationException`

--- dwcj-engine/src/main/java/org/dwcj/controls/tabcontrol/TabControl.java
+++ dwcj-engine/src/main/java/org/dwcj/controls/tabcontrol/TabControl.java
@@ -19,28 +19,26 @@ import java.util.function.Consumer;
 
 public final class TabControl extends AbstractDwcControl {
 
-    /**Event sink for selection of a tab */
+    /** Event sink for selection of a tab */
     private TabSelectEventSink tabSelectEventSink;
-    /**List of all callbacks to be added to the tab */
+    /** List of all callbacks to be added to the tab */
     private ArrayList<Consumer<TabSelectEvent>> callbacks = new ArrayList<>();
-    /**List of tabs associated with the control */
+    /** List of tabs associated with the control */
     private ArrayList<SimpleEntry<String, Div>> tabs = new ArrayList<>();
-    /**The tab control's theme */
+    /** The tab control's theme */
     private Theme theme = Theme.DEFAULT;
-    /**The tab control's expanse */
+    /** The tab control's expanse */
     private Expanse expanse = null;
-    /**The panel which the tab control belongs to */
+    /** The panel which the tab control belongs to */
     private AbstractPanel parentPanel;
-    /**The currently selected tab */
+    /** The currently selected tab */
     private int selected = 0;
-    /**Number of tabs in the control */
-    private int tabNum = 0;
 
-    public enum Expanse{
+    public enum Expanse {
         LARGE, MEDIUM, SMALL, XLARGE, XSMALL
     }
 
-    public enum Theme{
+    public enum Theme {
         DEFAULT, DANGER, GRAY, INFO, SUCCESS, WARNING
     }
 
@@ -51,82 +49,89 @@ public final class TabControl extends AbstractDwcControl {
         try {
             BBjWindow w = PanelAccessor.getDefault().getBBjWindow(p);
             parentPanel = p;
-            //todo: honor visibility flag, if set before adding the control to the form, so it's created invisibly right away
-            tabCtrl = w.addTabCtrl(w.getAvailableControlID(), BASISNUMBER_1, BASISNUMBER_1, BASISNUMBER_1, BASISNUMBER_1);
+            // todo: honor visibility flag, if set before adding the control to the form, so
+            // it's created invisibly right away
+            tabCtrl = w.addTabCtrl(w.getAvailableControlID(), BASISNUMBER_1, BASISNUMBER_1, BASISNUMBER_1,
+                    BASISNUMBER_1);
             ctrl = tabCtrl;
             catchUp();
-        } catch (Exception e)  {
+        } catch (Exception e) {
             Environment.logError(e);
         }
     }
 
-
     /**
      * Add a tab to the tab control
+     * 
      * @param text the text to display on the tab control
      * @return
      */
-    public TabControl add(String text){
+    public TabControl add(String text) {
 
-        if(this.ctrl != null){
+        if (this.ctrl != null) {
             try {
-                this.tabCtrl.addTab(text,-1);
+                this.tabCtrl.addTab(text, -1);
             } catch (BBjException e) {
                 Environment.logError(e);
             }
+        } else {
+            this.tabs.add(new SimpleEntry<>(text, null));
         }
-        this.tabs.add(new SimpleEntry<>(text, null));
-        tabNum++;
+
         return this;
     }
 
-
-
     /**
      * Add a tab and add a Div to it.
-     * Important: The DIV has to exist on the parent panel, you need to call "add" before passing it to the tab control.
-     * @param text The text for the tab
+     * Important: The DIV has to exist on the parent panel, you need to call "add"
+     * before passing it to the tab control.
+     * 
+     * @param text  The text for the tab
      * @param panel the panel to attach to the tab
      * @return the Tab Control object
      */
     public TabControl add(String text, Div panel) {
-        if(this.ctrl != null){
+        if (this.ctrl != null) {
             try {
                 parentPanel.add(panel);
-                this.tabCtrl.addTab(text,PanelAccessor.getDefault().getBBjWindow(panel));
+                this.tabCtrl.addTab(text, PanelAccessor.getDefault().getBBjWindow(panel));
             } catch (BBjException | IllegalAccessException e) {
                 Environment.logError(e);
             }
+        } else {
+            this.tabs.add(new SimpleEntry<>(text, panel));
         }
-        this.tabs.add(new SimpleEntry<>(text, panel));
-        tabNum++;
+
         return this;
     }
 
     /**
      * Returns the title of the tab at the given index
+     * 
      * @param index Desired index number
      * @return The title of the tab
      */
-    public String getTitleAt(int index){
+    public String getTitleAt(int index) {
         return this.tabs.get(index).getKey();
     }
-    
+
     /**
      * Returns the Div of the tab at the given index
+     * 
      * @param index Desired index number
      * @return The Div of the tab
      */
-    public Div getPanelAt(int index){
+    public Div getPanelAt(int index) {
         return this.tabs.get(index).getValue();
     }
 
     /**
      * Gets the number of tabs in the tab control
+     * 
      * @return The number of tabs
      */
-    public int getTabCount(){
-        if(this.ctrl != null){
+    public int getTabCount() {
+        if (this.ctrl != null) {
             try {
                 return this.tabCtrl.getNumTabs();
             } catch (BBjException e) {
@@ -143,8 +148,8 @@ public final class TabControl extends AbstractDwcControl {
      * @param title Title for the new tab
      * @return The control itself
      */
-    public TabControl insert(int index, String text){
-        if(this.ctrl != null){
+    public TabControl insert(int index, String text) {
+        if (this.ctrl != null) {
             try {
                 this.tabCtrl.insertTab(index, text, -1);
             } catch (BBjException e) {
@@ -152,7 +157,6 @@ public final class TabControl extends AbstractDwcControl {
             }
         }
         this.tabs.add(index, new SimpleEntry<>(text, null));
-        tabNum++;
         return this;
     }
 
@@ -164,8 +168,8 @@ public final class TabControl extends AbstractDwcControl {
      * @param panel Div to be associated with the new tab
      * @return The control itself
      */
-    public TabControl insert(int index, String text, Div panel){
-        if(this.ctrl != null){
+    public TabControl insert(int index, String text, Div panel) {
+        if (this.ctrl != null) {
             try {
                 this.tabCtrl.insertTab(index, text, PanelAccessor.getDefault().getBBjWindow(panel));
             } catch (BBjException | IllegalAccessException e) {
@@ -173,48 +177,47 @@ public final class TabControl extends AbstractDwcControl {
             }
         }
         this.tabs.add(index, new SimpleEntry<>(text, panel));
-        tabNum++;
         return this;
     }
 
     /**
      * Removes a tab from the tab control based on its index
+     * 
      * @param index Index of the tab designated for removal
      * @return The control itself
      */
-    public TabControl remove(int index){
-        if(this.ctrl != null){
-            try{
+    public TabControl remove(int index) {
+        if (this.ctrl != null) {
+            try {
                 App.consoleLog(String.valueOf(this.tabCtrl.getNumTabs()));
-            } catch(BBjException e){
+            } catch (BBjException e) {
                 Environment.logError(e);
             }
-            try{
+            try {
                 this.tabCtrl.removeTab(index);
-            } catch(BBjException e){
+            } catch (BBjException e) {
                 Environment.logError(e);
             }
         }
         this.tabs.remove(index);
-        tabNum--;
         return this;
     }
-    
-    public ArrayList<SimpleEntry<String, Div>> getTabs(){
+
+    public ArrayList<SimpleEntry<String, Div>> getTabs() {
         return this.tabs;
     }
 
-
-
     /**
      * Put a DIV under an existing tab
-     * Important: The DIV has to exist on the parent panel, you need to call "add" before passing it to the tab control.
+     * Important: The DIV has to exist on the parent panel, you need to call "add"
+     * before passing it to the tab control.
+     * 
      * @param index the zero-based index of the tab
      * @param panel the DIV panel to put under the tab
      * @return the Tab Control object itself
      */
     public TabControl setPanelAt(int index, Div panel) {
-        if(this.ctrl != null){
+        if (this.ctrl != null) {
             try {
                 this.tabCtrl.setControlAt(index, PanelAccessor.getDefault().getBBjWindow(panel));
             } catch (BBjException | IllegalAccessException e) {
@@ -227,22 +230,21 @@ public final class TabControl extends AbstractDwcControl {
 
     /**
      * Designates which of the tabs should be selected
+     * 
      * @param index Index of tab designated for selection
      * @return The control itself
      */
-    public TabControl selectIndex(int index){
-        if(this.ctrl != null){
-            try{
+    public TabControl selectIndex(int index) {
+        if (this.ctrl != null) {
+            try {
                 this.tabCtrl.setSelectedIndex(index);
-            } catch(BBjException e){
+            } catch (BBjException e) {
                 Environment.logError(e);
             }
         }
         this.selected = index;
         return this;
     }
-
-
 
     /**
      * register an event callback for the click event
@@ -252,18 +254,16 @@ public final class TabControl extends AbstractDwcControl {
      */
 
     public TabControl onSelect(Consumer<TabSelectEvent> callback) {
-        if(this.ctrl != null){
-            if(this.tabSelectEventSink == null){
+        if (this.ctrl != null) {
+            if (this.tabSelectEventSink == null) {
                 this.tabSelectEventSink = new TabSelectEventSink(this);
             }
             this.tabSelectEventSink.addCallback(callback);
-        }
-        else{
+        } else {
             this.callbacks.add(callback);
         }
         return this;
     }
-
 
     @Override
     public TabControl setText(String text) {
@@ -272,11 +272,11 @@ public final class TabControl extends AbstractDwcControl {
     }
 
     @Override
-    public TabControl setVisible(Boolean visible){
+    public TabControl setVisible(Boolean visible) {
         super.setVisible(visible);
         return this;
     }
-    
+
     @Override
     public TabControl setEnabled(Boolean enabled) {
         super.setEnabled(enabled);
@@ -290,13 +290,13 @@ public final class TabControl extends AbstractDwcControl {
     }
 
     @Override
-    public TabControl setAttribute(String attribute, String value){
-      super.setAttribute(attribute, value);
-      return this;
+    public TabControl setAttribute(String attribute, String value) {
+        super.setAttribute(attribute, value);
+        return this;
     }
 
     @Override
-    public TabControl setId(String elementId){
+    public TabControl setId(String elementId) {
         super.setId(elementId);
         return this;
     }
@@ -306,7 +306,7 @@ public final class TabControl extends AbstractDwcControl {
         super.setStyle(property, value);
         return this;
     }
-    
+
     @Override
     public TabControl addClassName(String selector) {
         super.addClassName(selector);
@@ -319,55 +319,52 @@ public final class TabControl extends AbstractDwcControl {
         return this;
     }
 
-
-
     public TabControl setExpanse(Expanse expanse) {
         super.setControlExpanse(expanse);
         return this;
     }
-
 
     public TabControl setTheme(Theme theme) {
         super.setControlTheme(theme);
         return this;
     }
 
- 
-
-
     @Override
     @SuppressWarnings("java:S3776") // tolerate cognitive complexity for now, it's just a batch list of checks
     protected void catchUp() throws IllegalAccessException {
-        if (Boolean.TRUE.equals(this.getCaughtUp())) throw new IllegalAccessException("catchUp cannot be called twice");
-        
+        if (Boolean.TRUE.equals(this.getCaughtUp()))
+            throw new IllegalAccessException("catchUp cannot be called twice");
+
         super.catchUp();
 
-        if(!this.callbacks.isEmpty()){
+        if (!this.callbacks.isEmpty()) {
             this.tabSelectEventSink = new TabSelectEventSink(this);
-            while(!this.callbacks.isEmpty()){
+            while (!this.callbacks.isEmpty()) {
                 this.tabSelectEventSink.addCallback(this.callbacks.remove(0));
             }
         }
 
-        /* Reimplemented logic instead of calling method to avoid duplicate tabs being added */
+        /*
+         * Reimplemented logic instead of calling method to avoid duplicate tabs being
+         * added
+         */
         tabs.forEach(n -> {
-            if(n.getValue() == null){
+            if (n.getValue() == null) {
                 this.add(n.getKey());
-            } 
-            else{
+            } else {
                 this.add(n.getKey(), n.getValue());
             }
         });
 
-        if(this.expanse != null){
+        if (this.expanse != null) {
             this.setExpanse(this.expanse);
         }
 
-        if(this.theme != Theme.DEFAULT){
+        if (this.theme != Theme.DEFAULT) {
             this.setTheme(this.theme);
         }
 
-        if(this.selected != 0){
+        if (this.selected != 0) {
             this.selectIndex(this.selected);
         }
 
