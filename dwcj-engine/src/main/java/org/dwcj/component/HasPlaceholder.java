a3a1244d35c0c02bb8d22973b2c273bee2bf22fa
Timon Geisbauer <47525540+TimonGeisbauer@users.noreply.github.com>
2023-04-20 11:41:40 +0200

    feat: added a HasPlaceholder interface (#304)
    * feat: added a HasPlaceholder interface
    * docs: update javadoc
    
    ---------
    
    Co-authored-by: Hyyan Abo Fakher <hyyanaf@gmail.com>

---
+++ dwcj-engine/src/main/java/org/dwcj/component/HasPlaceholder.java
@@ -0,0 +1,23 @@
+package org.dwcj.component;
+
+/**
+ * The HasPlaceholder interface provides methods to set and get the placeholder text 
+ * for a component that displays a placeholder when empty.
+ */
+public interface HasPlaceholder {
+
+  /**
+   * Sets the text to be displayed as the placeholder for the component.
+   *
+   * @param placeholder the text displayed
+   * @return the control itself
+   */
+  HasPlaceholder setPlaceholder(String placeholder);
+
+  /**
+   * Returns the placeholder text currently set for the control.
+   *
+   * @return the placeholder text
+   */
+  String getPlaceholder();
+}
