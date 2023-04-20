f3a28355e96df06a333f70edc2c2b8cacde0d998
Eric Handtke <ehandtke@basis.com>
2023-04-20 13:11:14 +0200

    chore: refactored events

--- dwcj-engine/src/main/java/org/dwcj/component/event/sink/MouseEnterEventSink.java
+++ dwcj-engine/src/main/java/org/dwcj/component/event/sink/MouseEnterEventSink.java
@@ -1,7 +1,5 @@
 package org.dwcj.component.event.sink;
 
-import com.basis.bbj.proxies.event.BBjEvent;
-import com.basis.bbj.proxies.event.BBjMouseEnterEvent;
 import com.basis.bbj.proxyif.SysGuiEventConstants;
 import org.dwcj.component.AbstractDwcComponent;
 import org.dwcj.component.event.EventDispatcher;
@@ -15,15 +13,4 @@ public class MouseEnterEventSink extends AbstractMouseEventSink {
     super(component, dispatcher, SysGuiEventConstants.ON_MOUSE_ENTER);
   }
 
-  /**
-   * Handles the BBj event and calls handleEventParams to map the 
-   * event parameters and dispatch a new {@link MouseEvent}.
-   *
-   * @param ev A BBj mouse event
-   */
-  @Override
-  public void handleEvent(BBjEvent ev) {
-    BBjMouseEnterEvent event = (BBjMouseEnterEvent) ev;
-    this.handleEventParams(event);
-  }
 }
--- dwcj-engine/src/main/java/org/dwcj/component/event/sink/RightMouseDownEventSink.java
+++ dwcj-engine/src/main/java/org/dwcj/component/event/sink/RightMouseDownEventSink.java
@@ -1,7 +1,6 @@
 package org.dwcj.component.event.sink;
 
-import com.basis.bbj.proxies.event.BBjEvent;
-import com.basis.bbj.proxies.event.BBjRightMouseDownEvent;
+
 import com.basis.bbj.proxyif.SysGuiEventConstants;
 import org.dwcj.component.AbstractDwcComponent;
 import org.dwcj.component.event.EventDispatcher;
@@ -16,15 +15,4 @@ public class RightMouseDownEventSink extends AbstractMouseEventSink {
     super(component, dispatcher, SysGuiEventConstants.ON_RIGHT_MOUSE_DOWN);
   }
 
-  /**
-   * Handles the BBj event and calls handleEventParams to map the 
-   * event parameters and dispatch a new {@link MouseEvent}.
-   *
-   * @param ev A BBj mouse event
-   */
-  @Override
-  public void handleEvent(BBjEvent ev) {
-    BBjRightMouseDownEvent event = (BBjRightMouseDownEvent) ev;
-    this.handleEventParams(event);
-  }
 }
