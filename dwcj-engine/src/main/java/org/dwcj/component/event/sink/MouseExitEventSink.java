bb06f72c207ef1154480dec55fc7afc294211e6f
Timon Geisbauer <tgeisbauer@basis.cloud>
2023-04-21 11:02:40 +0200

    feat: added a buildPayload method to the AbstractMouseEventSink and moved the handleEvent to the concrete MouseSink implementations

--- dwcj-engine/src/main/java/org/dwcj/component/event/sink/MouseExitEventSink.java
+++ dwcj-engine/src/main/java/org/dwcj/component/event/sink/MouseExitEventSink.java
@@ -1,16 +1,32 @@
 package org.dwcj.component.event.sink;
 
+import com.basis.bbj.proxies.event.BBjEvent;
+import com.basis.bbj.proxies.event.BBjMouseEvent;
 import com.basis.bbj.proxyif.SysGuiEventConstants;
+import java.util.HashMap;
 import org.dwcj.component.AbstractDwcComponent;
 import org.dwcj.component.event.EventDispatcher;
+import org.dwcj.component.event.MouseExitEvent;
 
 
 /**
- * This class will map the BBjMouseExitEvent event to a Java {@link MouseEvent}.
+ * This class will map the BBjMouseExitEvent event to a Java {@link MouseExitEvent}.
  */
 public class MouseExitEventSink extends AbstractMouseEventSink {
 
   public MouseExitEventSink(AbstractDwcComponent component, EventDispatcher dispatcher) {
     super(component, dispatcher, SysGuiEventConstants.ON_MOUSE_EXIT);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void handleEvent(BBjEvent ev) {
+    BBjMouseEvent event = (BBjMouseEvent) ev;
+    HashMap<String, Object> map = super.buildPayload(event);
+
+    MouseExitEvent dwcEv = new MouseExitEvent(component, map);
+    this.dispatcher.dispatchEvent(dwcEv);
   }
 }
--- dwcj-engine/src/main/java/org/dwcj/component/event/sink/RightMouseDownEventSink.java
+++ dwcj-engine/src/main/java/org/dwcj/component/event/sink/RightMouseDownEventSink.java
@@ -1,9 +1,12 @@
 package org.dwcj.component.event.sink;
 
+import com.basis.bbj.proxies.event.BBjEvent;
+import com.basis.bbj.proxies.event.BBjMouseEvent;
 import com.basis.bbj.proxyif.SysGuiEventConstants;
+import java.util.HashMap;
 import org.dwcj.component.AbstractDwcComponent;
 import org.dwcj.component.event.EventDispatcher;
-
+import org.dwcj.component.event.RightMouseDownEvent;
 
 /**
  * This class will map the BBjRightMouseDownEvent event to a Java {@link MouseEvent}.
@@ -13,4 +16,17 @@ public class RightMouseDownEventSink extends AbstractMouseEventSink {
   public RightMouseDownEventSink(AbstractDwcComponent component, EventDispatcher dispatcher) {
     super(component, dispatcher, SysGuiEventConstants.ON_RIGHT_MOUSE_DOWN);
   }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void handleEvent(BBjEvent ev) {
+    BBjMouseEvent event = (BBjMouseEvent) ev;
+    HashMap<String, Object> map = super.buildPayload(event);
+
+    RightMouseDownEvent dwcEv = new RightMouseDownEvent(component, map);
+    this.dispatcher.dispatchEvent(dwcEv);
+  }
+
 }
