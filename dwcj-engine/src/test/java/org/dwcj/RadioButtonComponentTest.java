4910848e0e5e7ca75d2b35e627ed7c59bc93df44
Oskan <oachmetsik@basis.cloud>
2023-04-24 14:17:57 +0200

    fix: checkStyle issues

--- dwcj-engine/src/test/java/org/dwcj/RadioButtonComponentTest.java
+++ dwcj-engine/src/test/java/org/dwcj/RadioButtonComponentTest.java
@@ -1,239 +1,223 @@
 package org.dwcj;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import javax.swing.GroupLayout.Alignment;
+
 import org.dwcj.component.radiobutton.RadioButton;
-import org.dwcj.component.radiobutton.RadioButton.Expanse;
 import org.dwcj.component.radiobutton.RadioButton.HorizontalTextPosition;
-import org.dwcj.component.radiobutton.event.RadioButtonCheckEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-import com.basis.bbj.processor.instruction.fa;
 
-/**.
- * 
+/**
+ * Class to test the RadioButton component.
  */
-public class RadioButtonComponentTest {
-    RadioButton radioButton;
+class RadioButtonComponentTest {
+  RadioButton radioButton;
 
-    @BeforeEach
-    void setUp() {
-        radioButton = new RadioButton();
-    }
+  @BeforeEach
+  void setUp() {
+    radioButton = new RadioButton();
+  }
 
-    @Test
-    @DisplayName("Id of RadioButton should be returned")
+  @Test
+  @DisplayName("Id of RadioButton should be returned")
     void testRadioButtonId() {
-        radioButton.setId("first");
-        assertEquals("first", radioButton.getId());
-    }
+    radioButton.setId("first");
+    assertEquals("first", radioButton.getId());
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Ensure correct handling of default value")
+  @RepeatedTest(5)
+  @DisplayName("Ensure correct handling of default value")
     void testDefaultRadioButtonId() {
-        assertEquals("", radioButton.getId());
-    }
+    assertEquals("", radioButton.getId());
+  }
 
-    @Test
-    @DisplayName("The RadioButton should be/not be checked")
+  @Test
+  @DisplayName("The RadioButton should be/not be checked")
     void testRadioButtonChecked() {
-        radioButton.setChecked(true);
-        assertEquals(true, radioButton.isChecked());
-        radioButton.setChecked(false);
-        assertEquals(false, radioButton.isChecked());
-    }
+    radioButton.setChecked(true);
+    assertEquals(true, radioButton.isChecked());
+    radioButton.setChecked(false);
+    assertEquals(false, radioButton.isChecked());
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Ensure correct handling of default value")
+  @RepeatedTest(5)
+  @DisplayName("Ensure correct handling of default value")
     void testDefaultRadioButtonChecked() {
-        assertEquals(false, radioButton.isChecked());
-    }
+    assertEquals(false, radioButton.isChecked());
+  }
 
-    @Test
-    @DisplayName("The RadioButton should be NOT editable")
+  @Test
+  @DisplayName("The RadioButton should be NOT editable")
     void testRadioButtonReadOnly() {
-        radioButton.setReadOnly(true);
-        assertEquals(true, radioButton.isReadOnly());
-    }
+    radioButton.setReadOnly(true);
+    assertEquals(true, radioButton.isReadOnly());
+  }
 
-    @Test
-    @DisplayName("The RadioButton should be/ not be focused")
+  @Test
+  @DisplayName("The RadioButton should be/ not be focused")
     void testRadioButtonFocusable() {
-        radioButton.setFocusable(false);
-        assertEquals(false, radioButton.isFocusable());
-        radioButton.setFocusable(true);
-        assertEquals(true, radioButton.isFocusable());
-    }
+    radioButton.setFocusable(false);
+    assertEquals(false, radioButton.isFocusable());
+    radioButton.setFocusable(true);
+    assertEquals(true, radioButton.isFocusable());
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Ensure correct handling of default value")
+  @RepeatedTest(5)
+  @DisplayName("Ensure correct handling of default value")
     void testDefaultRadioButtonFocusable() {
-        assertEquals(true, radioButton.isFocusable());
-    }
+    assertEquals(true, radioButton.isFocusable());
+  }
 
-    @Test
-    @DisplayName("The RadioButton should be/not be tabTraversable")
+  @Test
+  @DisplayName("The RadioButton should be/not be tabTraversable")
     void testRadioButtonTabTraversable() {
-        radioButton.setTabTraversable(false);
-        assertEquals(false, radioButton.isTabTraversable());
-        radioButton.setTabTraversable(true);
-        assertEquals(true, radioButton.isTabTraversable());
-    }
+    radioButton.setTabTraversable(false);
+    assertEquals(false, radioButton.isTabTraversable());
+    radioButton.setTabTraversable(true);
+    assertEquals(true, radioButton.isTabTraversable());
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Ensure correct handling of default value")
+  @RepeatedTest(5)
+  @DisplayName("Ensure correct handling of default value")
     void testDefaultRadioButtonTabTraversable() {
-        assertEquals(true, radioButton.isTabTraversable());
-    }
+    assertEquals(true, radioButton.isTabTraversable());
+  }
 
-    @Test
-    @DisplayName("The text of the RadioButton should be displayed")
+  @Test
+  @DisplayName("The text of the RadioButton should be displayed")
     void testRadioButtonText() {
-        radioButton.setText("myText");
-        assertEquals("myText", radioButton.getText());
-    }
+    radioButton.setText("myText");
+    assertEquals("myText", radioButton.getText());
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("The default text of RadioButton should be handled")
+  @RepeatedTest(5)
+  @DisplayName("The default text of RadioButton should be handled")
     void testDefaultRadioButtonText() {
-        assertEquals("", radioButton.getText());
-    }
+    assertEquals("", radioButton.getText());
+  }
 
-    @Test
-    @DisplayName("The RadioButton should be/not be visible")
+  @Test
+  @DisplayName("The RadioButton should be/not be visible")
     void testRadioButtonVisible() {
-        radioButton.setVisible(false);
-        assertEquals(false, radioButton.isVisible());
-        radioButton.setVisible(true);
-        assertEquals(true, radioButton.isVisible());
-    }
+    radioButton.setVisible(false);
+    assertEquals(false, radioButton.isVisible());
+    radioButton.setVisible(true);
+    assertEquals(true, radioButton.isVisible());
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Ensure correct handling of default value")
+  @RepeatedTest(5)
+  @DisplayName("Ensure correct handling of default value")
     void testDefaultRadioButtonVisible() {
-        assertEquals(true, radioButton.isVisible());
-    }
+    assertEquals(true, radioButton.isVisible());
+  }
 
-    @Test
-    @DisplayName("The RadioButton should be/not be enabled")
+  @Test
+  @DisplayName("The RadioButton should be/not be enabled")
     void testRadioButtonEnabled() {
-        radioButton.setEnabled(false);
-        assertEquals(false, radioButton.isEnabled());
-        radioButton.setEnabled(true);
-        assertEquals(true, radioButton.isEnabled());
-    }
+    radioButton.setEnabled(false);
+    assertEquals(false, radioButton.isEnabled());
+    radioButton.setEnabled(true);
+    assertEquals(true, radioButton.isEnabled());
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Ensure correct handling of default value")
+  @RepeatedTest(5)
+  @DisplayName("Ensure correct handling of default value")
     void testDefaultRadioButtonEnabled() {
-        assertEquals(true, radioButton.isEnabled());
-    }
+    assertEquals(true, radioButton.isEnabled());
+  }
 
-    @Test
-    @DisplayName("The RadioButton should show a tooltip")
+  @Test
+  @DisplayName("The RadioButton should show a tooltip")
     void testRadioButtonToolTip() {
-        radioButton.setTooltipText("myTool");
-        assertEquals("myTool", radioButton.getTooltipText());
-    }
+    radioButton.setTooltipText("myTool");
+    assertEquals("myTool", radioButton.getTooltipText());
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Ensure correct handling of default value")
+  @RepeatedTest(5)
+  @DisplayName("Ensure correct handling of default value")
     void testDefaultRadioButtonToolTip() {
-        assertEquals("", radioButton.getTooltipText());
-    }
+    assertEquals("", radioButton.getTooltipText());
+  }
 
-    @Test
-    @DisplayName("The RadioButton should have a userData")
+  @Test
+  @DisplayName("The RadioButton should have a userData")
     void testRadioButtonUserData() {
-        radioButton.setUserData("myKey", "myValue");
-        assertEquals("myValue", radioButton.getUserData("myKey"));
-    }
+    radioButton.setUserData("myKey", "myValue");
+    assertEquals("myValue", radioButton.getUserData("myKey"));
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Ensure correct handling of empty value")
+  @RepeatedTest(5)
+  @DisplayName("Ensure correct handling of empty value")
     void testDefaultRadioButtonUserData() {
-        assertEquals(null, radioButton.getUserData(""));
-    }
+    assertEquals(null, radioButton.getUserData(""));
+  }
 
-    @Test
-    @DisplayName("The RadioButton should return the attribute")
+  @Test
+  @DisplayName("The RadioButton should return the attribute")
     void testRadioButtonAttribute() {
-        radioButton.setAttribute("disabled", "true");
-        assertEquals(true, radioButton.isEnabled());
-    }
+    radioButton.setAttribute("disabled", "true");
+    assertEquals(true, radioButton.isEnabled());
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Ensure correct handling of empty value")
+  @RepeatedTest(5)
+  @DisplayName("Ensure correct handling of empty value")
     void testDefaultRadioButtonAttribute() {
-        assertEquals(null, radioButton.getAttribute(""));
-    }
+    assertEquals(null, radioButton.getAttribute(""));
+  }
 
-    @Test
-    @DisplayName("The RadioButton should set/remove style")
+  @Test
+  @DisplayName("The RadioButton should set/remove style")
     void testRadioButtonStyle() {
-        radioButton.setStyle("background", "red");
-        assertEquals("red", radioButton.getStyle("background"));
-        radioButton.removeStyle("background");
-        assertEquals(null, radioButton.getAttribute("background"));
-    }
+    radioButton.setStyle("background", "red");
+    assertEquals("red", radioButton.getStyle("background"));
+    radioButton.removeStyle("background");
+    assertEquals(null, radioButton.getAttribute("background"));
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Ensure correct handling of empty value")
+  @RepeatedTest(5)
+  @DisplayName("Ensure correct handling of empty value")
     void testDefaultRadioButtonStyle() {
-        assertEquals(null, radioButton.getAttribute(""));
-    }
+    assertEquals(null, radioButton.getAttribute(""));
+  }
 
-    @Test
-    @DisplayName("The RadioButton should set/remove className")
-    void testRadioButtonClassName() {
-        radioButton.addClassName("myId");
-        // cannot be tested yet
-    }
-
-    @Test
-    @DisplayName("Expanse of RadioButton should take an effect")
-    void testRadioButtonExpanse() {
-        radioButton.setExpanse(Expanse.MEDIUM);
-        // cannot be tested yet
-    }
-
-    @Test
-    @DisplayName("Text of RadioButton should be displayed in respectively horizontal position")
+  @Test
+  @DisplayName("Text of RadioButton should be displayed in respectively horizontal position")
     void testRadioButtonHorizontalText() {
-        radioButton.setHorizontalTextPosition(HorizontalTextPosition.RIGHT);
-        assertEquals(HorizontalTextPosition.RIGHT, radioButton.getHorizontalTextPosition());
-        radioButton.setHorizontalTextPosition(HorizontalTextPosition.LEFT);
-        assertEquals(HorizontalTextPosition.LEFT, radioButton.getHorizontalTextPosition());
-        radioButton.setHorizontalTextPosition(HorizontalTextPosition.LEADING);
-        assertEquals(HorizontalTextPosition.LEADING, radioButton.getHorizontalTextPosition());
-        radioButton.setHorizontalTextPosition(HorizontalTextPosition.TRAILING);
-        assertEquals(HorizontalTextPosition.TRAILING, radioButton.getHorizontalTextPosition());
-        radioButton.setHorizontalTextPosition(HorizontalTextPosition.CENTER);
-        assertEquals(HorizontalTextPosition.CENTER, radioButton.getHorizontalTextPosition());
-    }
+    radioButton.setHorizontalTextPosition(HorizontalTextPosition.RIGHT);
+    assertEquals(HorizontalTextPosition.RIGHT, radioButton.getHorizontalTextPosition());
+    radioButton.setHorizontalTextPosition(HorizontalTextPosition.LEFT);
+    assertEquals(HorizontalTextPosition.LEFT, radioButton.getHorizontalTextPosition());
+    radioButton.setHorizontalTextPosition(HorizontalTextPosition.LEADING);
+    assertEquals(HorizontalTextPosition.LEADING, radioButton.getHorizontalTextPosition());
+    radioButton.setHorizontalTextPosition(HorizontalTextPosition.TRAILING);
+    assertEquals(HorizontalTextPosition.TRAILING, radioButton.getHorizontalTextPosition());
+    radioButton.setHorizontalTextPosition(HorizontalTextPosition.CENTER);
+    assertEquals(HorizontalTextPosition.CENTER, radioButton.getHorizontalTextPosition());
+  }
 
-    @RepeatedTest(5)
-    @DisplayName("Ensure handling of defaul value")
+  @RepeatedTest(5)
+  @DisplayName("Ensure handling of defaul value")
     void testDefaulRadioButtonHorizontalText() {
-        assertEquals(HorizontalTextPosition.RIGHT, radioButton.getHorizontalTextPosition());
-    }
+    assertEquals(HorizontalTextPosition.RIGHT, radioButton.getHorizontalTextPosition());
+  }
 
-    @Test
-    @DisplayName("Event should be executed successfully")
+  @Test
+  @DisplayName("Event should be executed successfully")
     void testRadioButtonOnChange() {
-        radioButton.onChange(e -> {
-            radioButton.setText("Success");
-        });
-        assertEquals("Success", radioButton.getText());
-        // cannot be tested yet
-    }
+    radioButton.onChange(e -> {
+      radioButton.setText("Success");
+    });
+    assertEquals("Success", radioButton.getText());
+    // cannot be tested yet
+  }
 
-    @Test
-    @DisplayName("Checking if RadioButton is disabled")
+  @Test
+  @DisplayName("Checking if RadioButton is disabled")
     void testRadioButtonDisabled() {
-        radioButton.setEnabled(false);
-        assertEquals(true, radioButton.isDisabled());
-    }
+    radioButton.setEnabled(false);
+    assertEquals(true, radioButton.isDisabled());
+  }
 
 }
