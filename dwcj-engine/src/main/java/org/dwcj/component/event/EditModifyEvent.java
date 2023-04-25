3d9809e7a30a68989b246084ac72030f5a9d9a49
Timon Geisbauer <47525540+TimonGeisbauer@users.noreply.github.com>
2023-04-25 15:10:52 +0200

    feat: implement EditModifyEvent (#310)
    * feat: implemented the EditModifyEvent
    
    * docs: added javadoc to EditModifyEvents getText method
    
    * docs: Updated the class description comment
    
    ---------
    
    Co-authored-by: mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>

---
+++ dwcj-engine/src/main/java/org/dwcj/component/event/EditModifyEvent.java
@@ -0,0 +1,23 @@
+package org.dwcj.component.event;
+
+import java.util.Map;
+import org.dwcj.component.AbstractComponent;
+
+/**
+ * An event that is fired when a component is edited.
+ */
+public class EditModifyEvent extends Event<AbstractComponent> {
+
+  public EditModifyEvent(AbstractComponent component, Map<String, Object> payload) {
+    super(component, payload);
+  }
+  
+  /**
+   * Gets the text of the component that is sent as part of the event payload.
+   *
+   * @return the text of the component.
+   */
+  public String getText() {
+    return (String) this.getEventMap().get("text");
+  }
+}
