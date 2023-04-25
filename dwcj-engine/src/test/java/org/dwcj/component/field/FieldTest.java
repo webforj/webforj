da1678d3120fe2a81a136771ee3dd2039f552530
Timon Geisbauer <tgeisbauer@basis.cloud>
2023-04-25 12:31:06 +0200

    feat: created FieldTest file

---
+++ dwcj-engine/src/test/java/org/dwcj/component/field/FieldTest.java
@@ -0,0 +1,14 @@
+package org.dwcj.component.field;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class FieldTest {
+  Field field;
+
+
+  @BeforeEach
+  void setUp() {
+    field = new Field();
+  }
+    
+}
