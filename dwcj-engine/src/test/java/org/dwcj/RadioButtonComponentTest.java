498ec34306d18602f235a2b1a026eb575d7c909c
Oskan <oachmetsik@basis.cloud>
2023-04-25 13:48:25 +0200

    test: All tests successfully finished

--- dwcj-engine/src/test/java/org/dwcj/RadioButtonComponentTest.java
+++ dwcj-engine/src/test/java/org/dwcj/RadioButtonComponentTest.java
@@ -2,7 +2,9 @@ package org.dwcj;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.dwcj.component.TextAlignable.Alignment;
 import org.dwcj.component.radiobutton.RadioButton;
+import org.dwcj.component.radiobutton.RadioButton.Activation;
 import org.dwcj.component.radiobutton.RadioButton.HorizontalTextPosition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -22,20 +24,20 @@ class RadioButtonComponentTest {
 
   @Test
   @DisplayName("Id of RadioButton should be returned")
-    void testRadioButtonId() {
+  void testRadioButtonId() {
     radioButton.setId("first");
     assertEquals("first", radioButton.getId());
   }
 
   @RepeatedTest(5)
   @DisplayName("Ensure correct handling of default value")
-    void testDefaultRadioButtonId() {
+  void testDefaultRadioButtonId() {
     assertEquals("", radioButton.getId());
   }
 
   @Test
   @DisplayName("The RadioButton should be/not be checked")
-    void testRadioButtonChecked() {
+  void testRadioButtonChecked() {
     radioButton.setChecked(true);
     assertEquals(true, radioButton.isChecked());
     radioButton.setChecked(false);
@@ -44,20 +46,20 @@ class RadioButtonComponentTest {
 
   @RepeatedTest(5)
   @DisplayName("Ensure correct handling of default value")
-    void testDefaultRadioButtonChecked() {
+  void testDefaultRadioButtonChecked() {
     assertEquals(false, radioButton.isChecked());
   }
 
   @Test
   @DisplayName("The RadioButton should be NOT editable")
-    void testRadioButtonReadOnly() {
+  void testRadioButtonReadOnly() {
     radioButton.setReadOnly(true);
     assertEquals(true, radioButton.isReadOnly());
   }
 
   @Test
   @DisplayName("The RadioButton should be/ not be focused")
-    void testRadioButtonFocusable() {
+  void testRadioButtonFocusable() {
     radioButton.setFocusable(false);
     assertEquals(false, radioButton.isFocusable());
     radioButton.setFocusable(true);
@@ -66,13 +68,13 @@ class RadioButtonComponentTest {
 
   @RepeatedTest(5)
   @DisplayName("Ensure correct handling of default value")
-    void testDefaultRadioButtonFocusable() {
+  void testDefaultRadioButtonFocusable() {
     assertEquals(true, radioButton.isFocusable());
   }
 
   @Test
   @DisplayName("The RadioButton should be/not be tabTraversable")
-    void testRadioButtonTabTraversable() {
+  void testRadioButtonTabTraversable() {
     radioButton.setTabTraversable(false);
     assertEquals(false, radioButton.isTabTraversable());
     radioButton.setTabTraversable(true);
@@ -81,26 +83,26 @@ class RadioButtonComponentTest {
 
   @RepeatedTest(5)
   @DisplayName("Ensure correct handling of default value")
-    void testDefaultRadioButtonTabTraversable() {
+  void testDefaultRadioButtonTabTraversable() {
     assertEquals(true, radioButton.isTabTraversable());
   }
 
   @Test
   @DisplayName("The text of the RadioButton should be displayed")
-    void testRadioButtonText() {
+  void testRadioButtonText() {
     radioButton.setText("myText");
     assertEquals("myText", radioButton.getText());
   }
 
   @RepeatedTest(5)
   @DisplayName("The default text of RadioButton should be handled")
-    void testDefaultRadioButtonText() {
+  void testDefaultRadioButtonText() {
     assertEquals("", radioButton.getText());
   }
 
   @Test
   @DisplayName("The RadioButton should be/not be visible")
-    void testRadioButtonVisible() {
+  void testRadioButtonVisible() {
     radioButton.setVisible(false);
     assertEquals(false, radioButton.isVisible());
     radioButton.setVisible(true);
@@ -109,13 +111,13 @@ class RadioButtonComponentTest {
 
   @RepeatedTest(5)
   @DisplayName("Ensure correct handling of default value")
-    void testDefaultRadioButtonVisible() {
+  void testDefaultRadioButtonVisible() {
     assertEquals(true, radioButton.isVisible());
   }
 
   @Test
   @DisplayName("The RadioButton should be/not be enabled")
-    void testRadioButtonEnabled() {
+  void testRadioButtonEnabled() {
     radioButton.setEnabled(false);
     assertEquals(false, radioButton.isEnabled());
     radioButton.setEnabled(true);
@@ -124,52 +126,52 @@ class RadioButtonComponentTest {
 
   @RepeatedTest(5)
   @DisplayName("Ensure correct handling of default value")
-    void testDefaultRadioButtonEnabled() {
+  void testDefaultRadioButtonEnabled() {
     assertEquals(true, radioButton.isEnabled());
   }
 
   @Test
   @DisplayName("The RadioButton should show a tooltip")
-    void testRadioButtonToolTip() {
+  void testRadioButtonToolTip() {
     radioButton.setTooltipText("myTool");
     assertEquals("myTool", radioButton.getTooltipText());
   }
 
   @RepeatedTest(5)
   @DisplayName("Ensure correct handling of default value")
-    void testDefaultRadioButtonToolTip() {
+  void testDefaultRadioButtonToolTip() {
     assertEquals("", radioButton.getTooltipText());
   }
 
   @Test
   @DisplayName("The RadioButton should have a userData")
-    void testRadioButtonUserData() {
+  void testRadioButtonUserData() {
     radioButton.setUserData("myKey", "myValue");
     assertEquals("myValue", radioButton.getUserData("myKey"));
   }
 
   @RepeatedTest(5)
   @DisplayName("Ensure correct handling of empty value")
-    void testDefaultRadioButtonUserData() {
+  void testDefaultRadioButtonUserData() {
     assertEquals(null, radioButton.getUserData(""));
   }
 
   @Test
   @DisplayName("The RadioButton should return the attribute")
-    void testRadioButtonAttribute() {
+  void testRadioButtonAttribute() {
     radioButton.setAttribute("disabled", "true");
     assertEquals(true, radioButton.isEnabled());
   }
 
   @RepeatedTest(5)
   @DisplayName("Ensure correct handling of empty value")
-    void testDefaultRadioButtonAttribute() {
+  void testDefaultRadioButtonAttribute() {
     assertEquals(null, radioButton.getAttribute(""));
   }
 
   @Test
   @DisplayName("The RadioButton should set/remove style")
-    void testRadioButtonStyle() {
+  void testRadioButtonStyle() {
     radioButton.setStyle("background", "red");
     assertEquals("red", radioButton.getStyle("background"));
     radioButton.removeStyle("background");
@@ -178,13 +180,13 @@ class RadioButtonComponentTest {
 
   @RepeatedTest(5)
   @DisplayName("Ensure correct handling of empty value")
-    void testDefaultRadioButtonStyle() {
+  void testDefaultRadioButtonStyle() {
     assertEquals(null, radioButton.getAttribute(""));
   }
 
   @Test
   @DisplayName("Text of RadioButton should be displayed in respectively horizontal position")
-    void testRadioButtonHorizontalText() {
+  void testRadioButtonHorizontalText() {
     radioButton.setHorizontalTextPosition(HorizontalTextPosition.RIGHT);
     assertEquals(HorizontalTextPosition.RIGHT, radioButton.getHorizontalTextPosition());
     radioButton.setHorizontalTextPosition(HorizontalTextPosition.LEFT);
@@ -199,13 +201,13 @@ class RadioButtonComponentTest {
 
   @RepeatedTest(5)
   @DisplayName("Ensure handling of defaul value")
-    void testDefaulRadioButtonHorizontalText() {
+  void testDefaulRadioButtonHorizontalText() {
     assertEquals(HorizontalTextPosition.RIGHT, radioButton.getHorizontalTextPosition());
   }
 
   @Test
   @DisplayName("Event should be executed successfully")
-    void testRadioButtonOnChange() {
+  void testRadioButtonOnChange() {
     radioButton.onChange(e -> {
       radioButton.setText("Success");
     });
@@ -215,9 +217,77 @@ class RadioButtonComponentTest {
 
   @Test
   @DisplayName("Checking if RadioButton is disabled")
-    void testRadioButtonDisabled() {
+  void testRadioButtonDisabled() {
     radioButton.setEnabled(false);
     assertEquals(true, radioButton.isDisabled());
+  }
+
+  @Test
+  @DisplayName("Should return the enum value of Activation")
+  void testRadioButtonActivation() {
+    radioButton.setActivation(Activation.AUTO);
+    assertEquals(Activation.AUTO, radioButton.getActivation());
+  }
+
+  @RepeatedTest(5)
+  @DisplayName("Ensure handling of default value")
+  void testDefaultRadioButtonActivation() {
+    assertEquals(Activation.MANUAL, radioButton.getActivation());
+  }
+
+  @Test
+  @DisplayName("Should return the enum value of Expanse")
+  void testRadioButtonExpanse() {
+    radioButton.setExpanse(RadioButton.Expanse.SMALL);
+    assertEquals(RadioButton.Expanse.SMALL, radioButton.getExpanse());
+  }
+
+  @Test
+  @DisplayName("Should return the enum value of Alignment")
+  void testRadioButtonAlignment() {
+    radioButton.setTextAlignment(Alignment.RIGHT);
+    assertEquals(Alignment.RIGHT, radioButton.getTextAlignment());
+  }
+
+  @RepeatedTest(5)
+  @DisplayName("Ensure handling of defaul value")
+  void testDefaultRadioButtonAlignment() {
+    assertEquals(Alignment.MIDDLE, radioButton.getTextAlignment());
+  }
+
+  @Test
+  @DisplayName("Should check the validity of RadioButton")
+  void testRadioButtonInvalid() {
+    radioButton.setInvalid(true);
+    assertEquals(true, radioButton.isInvalid());
+  }
+
+  @Test
+  @DisplayName("Should check the invalid message of userInput")
+  void testRadioButtonInvalidMessage() {
+    radioButton.setInvalidMessage("hey");
+    assertEquals("hey", radioButton.getInvalidMessage());
+  }
+
+  @Test
+  @DisplayName("Should show the label of RadioButton") 
+  void testRadioButtonLabel() {
+    radioButton.setLabel("maLabel");
+    assertEquals("maLabel", radioButton.getLabel());
+  }
+
+  @Test
+  @DisplayName("A value is required or must be check for the form to be submittable")
+  void testRadioButtonRequired() {
+    radioButton.setRequired(true);
+    assertEquals(true, radioButton.isRequired());
+  }
+
+  @Test
+  @DisplayName("When true, the radio button will be rendered as a switch")
+  void testRadioButtonSwitch() {
+    radioButton.setSwitched(true);
+    assertEquals(true, radioButton.isSwitched());
   }
 
 }
