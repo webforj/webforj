100f2429d3de2aeb588115390358c0dd601d18f2
Oskan <oachmetsik@basis.cloud>
2023-04-26 17:07:03 +0200

    chore: implementation of base class with getSelecte/addRadioGroup methods

---
+++ dwcj-engine/src/main/java/org/dwcj/component/radiobutton/RadioGroup.java
@@ -0,0 +1,53 @@
+package org.dwcj.component.radiobutton;
+
+import com.basis.bbj.proxies.sysgui.BBjControl;
+import com.basis.bbj.proxies.sysgui.BBjRadioButton;
+import com.basis.bbj.proxies.sysgui.BBjRadioGroup;
+import com.basis.bbj.proxies.sysgui.BBjWindow;
+import com.basis.startup.type.BBjException;
+import com.basis.startup.type.BBjVector;
+import org.dwcj.Environment;
+import org.dwcj.bridge.WindowAccessor;
+import org.dwcj.component.AbstractDwcComponent;
+import org.dwcj.component.window.AbstractWindow;
+import org.dwcj.util.BBjFunctionalityHelper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RadioGroup extends AbstractDwcComponent{
+
+  private BBjRadioGroup bbjRadioGroup;
+  private List<RadioButton> radioButtonList = new ArrayList<>();
+
+  @Override
+  protected void create(AbstractWindow p) {
+    try {
+      BBjWindow w = WindowAccessor.getDefault().getBBjWindow(p);
+      ctrl = (BBjControl) w.addRadioGroup();
+      this.bbjRadioGroup = (BBjRadioGroup) ctrl;
+    } catch (Exception e) {
+      Environment.logError(e);
+    }
+  }
+
+  public RadioGroup addRadioGroup(RadioButton... buttons){
+    radioButtonList.addAll(List.of(buttons));
+    return this;
+  }
+
+  public RadioGroup addRadioGroup() {
+    return this;
+  }
+
+  public RadioButton getSelected() {
+    for (RadioButton radioButton : radioButtonList) {
+      if (radioButton.isChecked()) {
+        return radioButton;
+      }
+    }
+    return null;
+  }
+
+}
