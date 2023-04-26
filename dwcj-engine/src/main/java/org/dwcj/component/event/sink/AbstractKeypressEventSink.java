81aef8ac5704bdb8e056200cf0501333c06479db
Timon Geisbauer <47525540+TimonGeisbauer@users.noreply.github.com>
2023-04-26 15:24:30 +0200

    feat: implemented the KeypressEvent/Sink and Test (#309)
    * feat: implemented the KeypressEvent/Sink and Test
    
    * refactor: used primitiv types and renamed some methods
    
    * fix: KeypressEventSinkTest
    
    * feat: adjusted Keypress implementation
    
    * feat: create a FieldKeypressEvent and Sink and keept AbstractKeypressEventSink
    
    * feat: removed KeypressEventSink
    
    * feat: removed the FieldKeyPressEvent and replaced it with the KeyPressEvent
    
    * test: fix broken test FieldKeypressEventSinkTest
    
    ---------
    
    Co-authored-by: Hyyan Abo Fakher <hyyanaf@gmail.com>

---
+++ dwcj-engine/src/main/java/org/dwcj/component/event/sink/AbstractKeypressEventSink.java
@@ -0,0 +1,39 @@
+package org.dwcj.component.event.sink;
+
+import com.basis.bbj.proxies.event.BBjAbstractTextKeypressEvent;
+import com.basis.bbj.proxies.event.BBjEvent;
+import java.util.HashMap;
+import org.dwcj.component.AbstractDwcComponent;
+import org.dwcj.component.event.EventDispatcher;
+
+/**
+ * An abstract class of a keypress event sink which would handle a BBjKeypressEvent and dispatch the
+ * corresponding Java event.
+ */
+public abstract class AbstractKeypressEventSink extends AbstractSink {
+
+  protected AbstractKeypressEventSink(AbstractDwcComponent component, EventDispatcher dispatcher,
+      int sysGuiConstant) {
+    super(component, dispatcher, sysGuiConstant);
+  }
+
+  /**
+   * Helper method to build the payload for BBjKeyPressEvents.
+   *
+   * @param ev the event
+   * @return the payload
+   */
+  protected HashMap<String, Object> buildPayload(BBjEvent ev) {
+    HashMap<String, Object> map = new HashMap<>();
+    BBjAbstractTextKeypressEvent event = (BBjAbstractTextKeypressEvent) ev;
+
+    map.put("keyCode", event.getKeyCode());
+    map.put("altKey", event.isAltDown());
+    map.put("cmdKey", event.isCmdDown());
+    map.put("controlKey", event.isControlDown());
+    map.put("shiftKey", event.isShiftDown());
+
+    return map;
+  }
+
+}
