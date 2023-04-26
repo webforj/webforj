3aeecf25584c8de3eabb0ad75bfc8520b73e9767
Timon Geisbauer <tgeisbauer@basis.cloud>
2023-04-26 16:54:58 +0200

    feat: added Javadoc for the overriden methods from the AbstractDwcComponent and added the catchup

--- dwcj-engine/src/main/java/org/dwcj/component/tree/Tree.java
+++ dwcj-engine/src/main/java/org/dwcj/component/tree/Tree.java
@@ -516,60 +516,131 @@ public final class Tree extends AbstractDwcComponent {
     return this;
   }
 
-
-
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Tree setText(String text) {
     super.setText(text);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Tree setVisible(Boolean visible) {
     super.setVisible(visible);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Tree setEnabled(Boolean enabled) {
     super.setEnabled(enabled);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Tree setTooltipText(String text) {
     super.setTooltipText(text);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Tree setAttribute(String attribute, String value) {
     super.setAttribute(attribute, value);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Tree removeAttribute(String attribute) {
+    super.removeAttribute(attribute);
+    return this;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Tree setId(String elementId) {
     super.setId(elementId);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Tree setUserData(String key, Object userData) {
+    super.setUserData(key, userData);
+    return this;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Tree setStyle(String property, String value) {
     super.setStyle(property, value);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Tree removeStyle(String property) {
+    super.removeStyle(property);
+    return this;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Tree addClassName(String selector) {
     super.addClassName(selector);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Tree removeClassName(String selector) {
     super.removeClassName(selector);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Tree setProperty(String property, Object value) {
+    super.setProperty(property, value);
+    return this;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void catchUp() throws IllegalAccessException {
+    if (Boolean.TRUE.equals(this.getCaughtUp())) {
+      throw new IllegalAccessException("catchUp cannot be called twice");
+    }
+    super.catchUp();
+  }
 }
