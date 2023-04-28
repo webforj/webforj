2e9fec651efea849d5fa5ad31ca98ed21d836c3f
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-04-28 10:00:39 +0200

    docs: Updated the documentation for focus method

--- dwcj-engine/src/main/java/org/dwcj/component/AbstractDwcComponent.java
+++ dwcj-engine/src/main/java/org/dwcj/component/AbstractDwcComponent.java
@@ -435,6 +435,13 @@ public abstract class AbstractDwcComponent extends AbstractComponent implements 
     return this;
   }
 
+  /**
+  * Gives a component focus when it is added to the window. Note that if this
+  * method is called on multiple components, focus will be given to the component
+  * added latest to the window. 
+  *
+  * @return The component itself
+  */
   protected AbstractDwcComponent focusComponent() throws DwcjRuntimeException {
     if (this.ctrl != null) {
       try {
