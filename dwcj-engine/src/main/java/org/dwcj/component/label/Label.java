6715d4a7fe8e161ea43084ca984eec069fdb1247
EHandtke <47521596+EHandtke@users.noreply.github.com>
2023-04-28 15:55:44 +0200

    feat: complete Label component implantation (#312)
    * fix: remove unwanted methods
    
    * feat: added event methods
    
    * fix: adjusted the used addStaticText method
    
    * feat: added textAlignable and linewrap methods
    
    * chore: added java doc inheritance to override, cleaned up whitespaces
    
    * feat: added constructors and refactored method names, removed deprecated method
    
    * feat: added dwcjruntimeexceptions instead of suppressing bbjexceptions
    
    * refactor: remove unrequired import
    
    * refactor: remove wrap constructor
    
    * refactor: `ìsWrap` should return `boolean`
    
    * refactor: remove unrequired comment
    
    * feat: removed HasEnable interface from AbstractDwcComponent (#325)
    
    * feat: removed hasEnable interface from abstractdwcControl, added to components
    
    * fix: removed hasEnable from components who shouldn't have it
    
    * chore: replaced Boolean with primitive boolean
    
    * fix: readded wrongfully removed interface to frame
    
    * feat: renamed util to utilities (#330)
    
    * feat: added componentTabTraversable helper methods to AbstractDwcComponent(#329)
    
    * feat: added componentTabTraversable helper methods to abstractdwcjcomponent
    
    * docs: Added and modified some of the method docs
    
    * fix: passed value to method instead of false
    
    ---------
    
    Co-authored-by: mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
    
    * feat: removed Focusable interface and implemented HasFocus. (#327)
    
    * feat: removed Focusable interface and replaced with hasFocus. Added helpermethod in AbstractDWCJComponent to call focus()
    
    * feat: removed HasEnable interface from AbstractDwcComponent (#325)
    
    * feat: removed hasEnable interface from abstractdwcControl, added to components
    
    * fix: removed hasEnable from components who shouldn't have it
    
    * chore: replaced Boolean with primitive boolean
    
    * fix: readded wrongfully removed interface to frame
    
    * feat: renamed util to utilities (#330)
    
    * docs: Updated the documentation for focus method
    
    * docs: Updated method javadoc
    
    * feat: added componentTabTraversable helper methods to AbstractDwcComponent(#329)
    
    * feat: added componentTabTraversable helper methods to abstractdwcjcomponent
    
    * docs: Added and modified some of the method docs
    
    * fix: passed value to method instead of false
    
    ---------
    
    Co-authored-by: mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
    
    * fix: resolved conflicts preparing for merge
    
    * docs: Updated the documentation for focus method
    
    * docs: Updated method javadoc
    
    ---------
    
    Co-authored-by: Timon Geisbauer <47525540+TimonGeisbauer@users.noreply.github.com>
    Co-authored-by: mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
    
    * style: fix formatting
    
    * fix: included events in catchup
    
    * chore:fixed formatting, lints and javadoc
    
    ---------
    
    Co-authored-by: Hyyan Abo Fakher <hyyanaf@gmail.com>
    Co-authored-by: Timon Geisbauer <47525540+TimonGeisbauer@users.noreply.github.com>
    Co-authored-by: mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>

--- dwcj-engine/src/main/java/org/dwcj/component/label/Label.java
+++ dwcj-engine/src/main/java/org/dwcj/component/label/Label.java
@@ -3,116 +3,338 @@ package org.dwcj.component.label;
 import com.basis.bbj.proxies.sysgui.BBjStaticText;
 import com.basis.bbj.proxies.sysgui.BBjWindow;
 import com.basis.startup.type.BBjException;
-
-import org.dwcj.Environment;
 import org.dwcj.bridge.WindowAccessor;
 import org.dwcj.component.AbstractDwcComponent;
-import org.dwcj.component.HasEnable;
 import org.dwcj.component.TextAlignable;
+import org.dwcj.component.event.EventDispatcher;
+import org.dwcj.component.event.EventListener;
+import org.dwcj.component.event.MouseEnterEvent;
+import org.dwcj.component.event.MouseExitEvent;
+import org.dwcj.component.event.RightMouseDownEvent;
+import org.dwcj.component.event.sink.MouseEnterEventSink;
+import org.dwcj.component.event.sink.MouseExitEventSink;
+import org.dwcj.component.event.sink.RightMouseDownEventSink;
 import org.dwcj.component.window.AbstractWindow;
+import org.dwcj.exceptions.DwcjRuntimeException;
 import org.dwcj.utilities.BBjFunctionalityHelper;
 
+
+/** A label object. */
 public final class Label extends AbstractDwcComponent implements TextAlignable {
 
-  /*
-   * top, right, bottom, left margins, used simple array as user will not have to interact directly
-   * with this
-   */
+  private EventDispatcher dispatcher = new EventDispatcher();
+  private MouseEnterEventSink mouseEnterEventSink;
+  private MouseExitEventSink mouseExitEventSink;
+  private RightMouseDownEventSink rightMouseDownEventSink;
+  private boolean lineWrap = true;
 
+  /**
+   * Default Constructor to automatically create an empty label.
+   */
   public Label() {
     this("");
   }
 
   /**
-   * Constructor used to give the label initial text
+   * Constructor used to give the label initial text.
    *
    * @param text String value for initial display text
    */
   public Label(String text) {
     setText(text);
-    this.textAlignment = Alignment.LEFT;
   }
 
+  /**
+   * Constructor used to give the label initial text and wether it is linewrapped or not.
+   *
+   * @param text String value for initial display text
+   * @param wrap Boolean value for linewrapping.
+   */
+  public Label(String text, boolean wrap) {
+    this(text);
+    this.setWrap(wrap);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  protected void create(AbstractWindow p) {
+  protected void create(AbstractWindow p) throws DwcjRuntimeException {
     try {
       BBjWindow w = WindowAccessor.getDefault().getBBjWindow(p);
       byte[] flags = BBjFunctionalityHelper.buildStandardCreationFlags(this.isVisible(), true);
-      ctrl = w.addStaticText(w.getAvailableControlID(), BASISNUMBER_1, BASISNUMBER_1, BASISNUMBER_1,
-          BASISNUMBER_1, getText(), flags);
+      ctrl = w.addStaticText(getText(), flags);
+      this.mouseEnterEventSink = new MouseEnterEventSink(this, dispatcher);
+      this.mouseExitEventSink = new MouseExitEventSink(this, dispatcher);
+      this.rightMouseDownEventSink = new RightMouseDownEventSink(this, dispatcher);
       catchUp();
     } catch (Exception e) {
-      Environment.logError(e);
+      throw new DwcjRuntimeException(e);
     }
   }
 
-  @Override
-  public Alignment getTextAlignment() {
-    return this.textAlignment;
+  /**
+   * Adds a MouseEnter event for the Label component.
+   *
+   * @param listener the event listener to be added
+   * @return The Label itself
+   */
+  public Label addMouseEnterListener(EventListener<MouseEnterEvent> listener) {
+    if (this.ctrl != null && this.dispatcher.getListenersCount(MouseEnterEvent.class) == 0) {
+      this.mouseEnterEventSink.setCallback();
+    }
+    dispatcher.addEventListener(MouseEnterEvent.class, listener);
+    return this;
   }
 
-  @Override
-  public Label setTextAlignment(Alignment alignment) {
+  /**
+   * Alias for the addMouseEnterListener method.
+   *
+   * @see Label#addMouseEnterListener(EventListener)
+   * @param listener the event listener to be added
+   * @return The Label itself
+   */
+  public Label onMouseEnter(EventListener<MouseEnterEvent> listener) {
+    return addMouseEnterListener(listener);
+  }
+
+  /**
+   * Removes a MouseEnter event from the Label component.
+   *
+   * @param listener the event listener to be removed
+   * @return The Label itself
+   */
+  public Label removeMouseEnterListener(EventListener<MouseEnterEvent> listener) {
+    dispatcher.removeEventListener(MouseEnterEvent.class, listener);
+    if (this.ctrl != null && this.dispatcher.getListenersCount(MouseEnterEvent.class) == 0) {
+      this.mouseEnterEventSink.removeCallback();
+    }
+    return this;
+  }
+
+  /**
+   * Adds a MouseExit event for the Label component.
+   *
+   * @param listener the event listener to be added
+   * @return The Label itself
+   */
+  public Label addMouseExitListener(EventListener<MouseExitEvent> listener) {
+    if (this.ctrl != null && this.dispatcher.getListenersCount(MouseExitEvent.class) == 0) {
+      this.mouseExitEventSink.setCallback();
+    }
+    dispatcher.addEventListener(MouseExitEvent.class, listener);
+    return this;
+  }
+
+  /**
+   * Alias for the addMouseExitListener method.
+   *
+   * @see Label#addMouseExitListener(EventListener)
+   * @param listener the event listener to be added
+   * @return The Label itself
+   */
+  public Label onMouseExit(EventListener<MouseExitEvent> listener) {
+    return addMouseExitListener(listener);
+  }
+
+  /**
+   * Removes a MouseExit event from the Label component.
+   *
+   * @param listener the event listener to be removed
+   * @return The Label itself
+   */
+  public Label removeMouseExitListener(EventListener<MouseExitEvent> listener) {
+    dispatcher.removeEventListener(MouseExitEvent.class, listener);
+    if (this.ctrl != null && this.dispatcher.getListenersCount(MouseExitEvent.class) == 0) {
+      this.mouseExitEventSink.removeCallback();
+    }
+    return this;
+  }
+
+  /**
+   * Adds a MouseExit event for the Label component.
+   *
+   * @param listener the event listener to be added
+   * @return The Label itself
+   */
+  public Label addRightMouseDownListener(EventListener<RightMouseDownEvent> listener) {
+    if (this.ctrl != null && this.dispatcher.getListenersCount(RightMouseDownEvent.class) == 0) {
+      this.rightMouseDownEventSink.setCallback();
+    }
+    dispatcher.addEventListener(RightMouseDownEvent.class, listener);
+    return this;
+  }
+
+  /**
+   * Alias for the addRightMouseDownListener method.
+   *
+   * @see Label#addRightMouseDownListener(EventListener)
+   * @param listener the event listener to be added
+   * @return The Label itself
+   */
+  public Label onRightMouseDown(EventListener<RightMouseDownEvent> listener) {
+    return addRightMouseDownListener(listener);
+  }
+
+  /**
+   * Removes a RightMouseDown event from the Label component.
+   *
+   * @param listener the event listener to be removed
+   * @return The Label itself
+   */
+  public Label removeRightMouseDownListener(EventListener<RightMouseDownEvent> listener) {
+    dispatcher.removeEventListener(RightMouseDownEvent.class, listener);
+    if (this.ctrl != null && this.dispatcher.getListenersCount(RightMouseDownEvent.class) == 0) {
+      this.rightMouseDownEventSink.removeCallback();
+    }
+    return this;
+  }
+
+  /**
+   * Sets whether the lines will be wrapped in the label.
+   *
+   * @param wrap - Specifies whether the lines will be wrapped (false = Not Wrapped, true = Wrapped)
+   * @return Returns this
+   */
+  public Label setWrap(Boolean wrap) throws DwcjRuntimeException {
     if (this.ctrl != null) {
       try {
-        ((BBjStaticText) ctrl).setAlignment(alignment.textPosition);
+        ((BBjStaticText) this.ctrl).setLineWrap(wrap);
       } catch (BBjException e) {
-        Environment.logError(e);
+        throw new DwcjRuntimeException(e);
+      }
+    }
+    this.lineWrap = wrap;
+    return this;
+  }
+
+  /**
+   * Returns whether lines are wrapped in the label.
+   *
+   * @return Returns whether the lines are wrapped in the component (false = Not Wrapped, true =
+   *         Wrapped).
+   */
+  public boolean isWrap() {
+    return this.lineWrap;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Label setTextAlignment(Alignment alignment) throws DwcjRuntimeException {
+    if (this.ctrl != null) {
+      try {
+        ((BBjStaticText) this.ctrl).setAlignment(alignment.textPosition);
+      } catch (BBjException e) {
+        throw new DwcjRuntimeException(e);
       }
     }
     this.textAlignment = alignment;
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Alignment getTextAlignment() {
+    return this.textAlignment;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Label setText(String text) {
     super.setText(text);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Label setVisible(Boolean visible) {
     super.setVisible(visible);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Label setTooltipText(String text) {
     super.setTooltipText(text);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Label setAttribute(String attribute, String value) {
     super.setAttribute(attribute, value);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Label setStyle(String property, String value) {
     super.setStyle(property, value);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Label addClassName(String selector) {
     super.addClassName(selector);
     return this;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Label removeClassName(String selector) {
     super.removeClassName(selector);
     return this;
   }
 
-  @Override
-  @SuppressWarnings("java:S3776") // tolerate cognitive complexity for now, it's just a batch list
-                                  // of checks
-  protected void catchUp() throws IllegalAccessException {
-    if (Boolean.TRUE.equals(this.getCaughtUp()))
-      throw new IllegalAccessException("catchUp cannot be called twice");
-    super.catchUp();
+  private void eventCatchUp() {
+    if (this.dispatcher.getListenersCount(MouseEnterEvent.class) > 0) {
+      this.mouseEnterEventSink.setCallback();
+    }
+
+    if (this.dispatcher.getListenersCount(MouseExitEvent.class) > 0) {
+      this.mouseExitEventSink.setCallback();
+    }
+
+    if (this.dispatcher.getListenersCount(RightMouseDownEvent.class) > 0) {
+      this.rightMouseDownEventSink.setCallback();
+    }
   }
 
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void catchUp() throws IllegalAccessException {
+    if (Boolean.TRUE.equals(this.getCaughtUp())) {
+      throw new IllegalAccessException("catchUp cannot be called twice");
+    }
+    super.catchUp();
+    eventCatchUp();
+    if (!this.lineWrap) {
+      this.setWrap(lineWrap);
+    }
+
+    if (this.textAlignment != Alignment.MIDDLE) {
+      this.setTextAlignment(this.textAlignment);
+    }
+  }
 }
