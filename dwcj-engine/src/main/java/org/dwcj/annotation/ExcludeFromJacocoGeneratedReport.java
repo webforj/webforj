01ca3896a40566b49182205fff3e7156c7f203d5
Hyyan Abo Fakher <hyyanaf@gmail.com>
2023-04-30 16:13:12 +0200

    feat: add `ExcludeFromJacocoGeneratedReport` annotation

---
+++ dwcj-engine/src/main/java/org/dwcj/annotation/ExcludeFromJacocoGeneratedReport.java
@@ -0,0 +1,16 @@
+package org.dwcj.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to exclude a method from the jacoco generated report.
+ *
+ * @author Hyyan Abo Fakher
+ **/
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ExcludeFromJacocoGeneratedReport {
+}
