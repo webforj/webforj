c8ddee8a22ef1cf5a145929faa7636bf758d3927
Timon Geisbauer <47525540+TimonGeisbauer@users.noreply.github.com>
2023-05-08 09:29:18 +0200

    feat: added a VerticalAlignment interface

---
+++ dwcj-engine/src/main/java/org/dwcj/component/VerticalAlignment.java
@@ -0,0 +1,46 @@
+package org.dwcj.component;
+
+/**
+ * On applicable controls, creates enum which helps facilitate underlying BBj constant integers for
+ * vertical alignment behavior to legible enum values, and facilitates implementation of methods 
+ * to interact with this behavior.
+ */
+public interface VerticalAlignment {
+
+  /**
+   * Enum which represents the text alignment of a component.
+   */
+  enum Alignment {
+    TOP(8192), CENTER(16384), BOTTOM(32768);
+
+    public final int value;
+
+    private Alignment(int position) {
+      this.value = position;
+    }
+
+    /**
+     * Gets the integer value of the text alignment.
+     *
+     * @return Integer value of text alignment
+     */
+    public int getValue() {
+      return value;
+    }
+  }
+
+  /**
+   * Sets the vertical alignment of the text within the control.
+   *
+   * @param alignment Enum from list representing an internal BBj numeric constant
+   * @return the control itself
+   */
+  public VerticalAlignment setVerticalAlignment(Alignment alignment);
+
+  /**
+   * Returns a value indication the text's vertical alignment.
+   *
+   * @return Enum value of text alignment
+   */
+  public Alignment getVerticalAlignment();
+}
