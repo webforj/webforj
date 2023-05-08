86101a7a0888f682c0f706db530b33c223e523a5
Timon Geisbauer <47525540+TimonGeisbauer@users.noreply.github.com>
2023-05-08 14:58:28 +0200

    feat: added HasBeep interface (#344)
    * feat: added HasBeep interface
    
    * docs: Slight changes to the Javadoc
    
    ---------
    
    Co-authored-by: mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>

---
+++ dwcj-engine/src/main/java/org/dwcj/component/HasBeep.java
@@ -0,0 +1,22 @@
+package org.dwcj.component;
+
+/**
+ * Interface for a component that can beep when clicked.
+ */
+public interface HasBeep {
+
+  /**
+   * Set whether the component should beep when clicked.
+   *
+   * @param beep true if the control should beep, false otherwise
+   * @return the control itself
+   */
+  public HasBeep setBeep(boolean beep);
+
+  /**
+   * Returns whether the component beeps.
+   *
+   * @return true if the component beeps, false otherwise
+   */
+  public boolean getBeep();
+}
