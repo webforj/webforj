4d07c1e7c9dd0841af6fdd3a6d562f05d20261ea
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-05-12 11:25:44 +0200

    docs: updated description for class and method

--- dwcj-engine/src/main/java/org/dwcj/component/event/ToggleEvent.java
+++ dwcj-engine/src/main/java/org/dwcj/component/event/ToggleEvent.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import org.dwcj.component.AbstractComponent;
 
 /**
- * An event that is fired when a component is checked on or off.
+ * An event that is fired when a component is toggled on or off.
  */
 public class ToggleEvent extends Event<AbstractComponent> {
   public ToggleEvent(AbstractComponent component, Map<String, Object> payload) {
@@ -12,9 +12,9 @@ public class ToggleEvent extends Event<AbstractComponent> {
   }
 
   /**
-   * Gets the value of the component to determine wether it was checked on or off.
+   * Gets the value of the component to determine whether it was toggled on or off.
    *
-   * @return a boolean representing wether it was checked on or off.
+   * @return a boolean returns true if toggled on, false if toggled off.
    */
   public boolean isToggled() {
     return (boolean) this.getEventMap().get("toggled");
