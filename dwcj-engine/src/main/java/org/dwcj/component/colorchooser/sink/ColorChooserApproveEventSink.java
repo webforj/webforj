f903aa0a665e37fbf2cff61db5f231e8220e4f1c
Oskan <oachmetsik@basis.cloud>
2023-05-31 16:45:10 +0200

    chore: Implementing ApproveEventSink for ColorChooser

--- dwcj-engine/src/main/java/org/dwcj/component/colorchooser/sink/ColorChooserApproveEventSink.java
+++ dwcj-engine/src/main/java/org/dwcj/component/colorchooser/sink/ColorChooserApproveEventSink.java
@@ -1,2 +1,24 @@
-package org.dwcj.component.colorchooser.sink;public class ColorChooserApproveEventSink {
+package org.dwcj.component.colorchooser.sink;
+
+import com.basis.bbj.proxies.event.BBjEvent;
+import com.basis.bbj.proxyif.SysGuiEventConstants;
+import org.dwcj.component.AbstractDwcComponent;
+import org.dwcj.component.colorchooser.event.ColorChooserApproveEvent;
+import org.dwcj.component.event.EventDispatcher;
+import org.dwcj.component.event.sink.AbstractSink;
+
+import java.util.HashMap;
+
+public class ColorChooserApproveEventSink extends AbstractSink {
+
+  public ColorChooserApproveEventSink(AbstractDwcComponent component, EventDispatcher dispatcher) {
+    super(component, dispatcher, SysGuiEventConstants.ON_COLORCHOOSER_APPROVE);
+  }
+
+  public void handleEvent(BBjEvent event) {
+    HashMap<String, Object> map = new HashMap<>();
+    ColorChooserApproveEvent dwcEv = new ColorChooserApproveEvent(component, map);
+    this.dispatcher.dispatchEvent(dwcEv);
+  }
+
 }
