08ee4a315f6be4c2f002236f8185a0a385f2c492
Oskan <oachmetsik@basis.cloud>
2023-05-31 17:09:39 +0200

    chore: Implementing ColorChooserCancelEVENT

--- dwcj-engine/src/main/java/org/dwcj/component/colorchooser/event/ColorChooserCancelEvent.java
+++ dwcj-engine/src/main/java/org/dwcj/component/colorchooser/event/ColorChooserCancelEvent.java
@@ -1,2 +1,13 @@
-package org.dwcj.component.colorchooser.event;public class ColorChooserCancelEvent {
+package org.dwcj.component.colorchooser.event;
+
+import org.dwcj.component.AbstractComponent;
+import org.dwcj.component.event.Event;
+
+import java.util.Map;
+
+public class ColorChooserCancelEvent extends Event<AbstractComponent> {
+
+  public ColorChooserCancelEvent(AbstractComponent component, Map<String, Object> payload) {
+    super(component, payload);
+  }
 }
