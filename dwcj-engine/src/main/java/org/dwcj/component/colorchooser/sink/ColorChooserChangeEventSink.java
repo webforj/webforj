7c647395b394f104dc1e4592161a19c1065d8825
Oskan <oachmetsik@basis.cloud>
2023-06-01 13:38:11 +0200

    chore: Implementing ColorChooserChangeEventSINK

--- dwcj-engine/src/main/java/org/dwcj/component/colorchooser/sink/ColorChooserChangeEventSink.java
+++ dwcj-engine/src/main/java/org/dwcj/component/colorchooser/sink/ColorChooserChangeEventSink.java
@@ -1,2 +1,29 @@
-package org.dwcj.component.colorchooser.sink;public class ColorChooserChangeEventSink {
+package org.dwcj.component.colorchooser.sink;
+
+import com.basis.bbj.proxies.event.BBjColorChooserChangeEvent;
+import com.basis.bbj.proxies.event.BBjEvent;
+import com.basis.bbj.proxyif.SysGuiEventConstants;
+import com.basis.startup.type.BBjException;
+import org.dwcj.component.AbstractDwcComponent;
+import org.dwcj.component.colorchooser.event.ColorChooserApproveEvent;
+import org.dwcj.component.event.EventDispatcher;
+import org.dwcj.component.event.sink.AbstractSink;
+
+import java.util.HashMap;
+
+public class ColorChooserChangeEventSink extends AbstractSink {
+
+  public ColorChooserChangeEventSink(AbstractDwcComponent component, EventDispatcher dispatcher) {
+    super(component, dispatcher, SysGuiEventConstants.ON_COLORCHOOSER_CHANGE);
+  }
+
+  public void handleEvent(BBjEvent event) throws BBjException {
+    BBjColorChooserChangeEvent ev = (BBjColorChooserChangeEvent) event;
+    HashMap<String, Object> map = new HashMap<>();
+
+    map.put("color", ev.getColor());
+
+    ColorChooserApproveEvent dwcEv = new ColorChooserApproveEvent(component, map);
+    this.dispatcher.dispatchEvent(dwcEv);
+  }
 }
