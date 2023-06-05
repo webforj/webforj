c65ef55256b42bf5175c00dd4fac90fcdce9c6ae
Oskan <oachmetsik@basis.cloud>
2023-06-05 16:54:04 +0200

    doc: Events added for CancelEvent

--- dwcj-engine/src/main/java/org/dwcj/component/colorchooser/event/ColorChooserCancelEvent.java
+++ dwcj-engine/src/main/java/org/dwcj/component/colorchooser/event/ColorChooserCancelEvent.java
@@ -1,10 +1,12 @@
 package org.dwcj.component.colorchooser.event;
 
+import java.util.Map;
 import org.dwcj.component.AbstractComponent;
 import org.dwcj.component.event.Event;
 
-import java.util.Map;
-
+/**
+ * Cancel Event for the ColorChooser Component.
+ */
 public class ColorChooserCancelEvent extends Event<AbstractComponent> {
 
   public ColorChooserCancelEvent(AbstractComponent component, Map<String, Object> payload) {
