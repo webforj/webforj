8103347c795122f275980eda7cf89ee536dbf9bf
Oskan <oachmetsik@basis.cloud>
2023-06-05 16:55:15 +0200

    doc: Docs added for ChangeEvent

--- dwcj-engine/src/main/java/org/dwcj/component/colorchooser/event/ColorChooserChangeEvent.java
+++ dwcj-engine/src/main/java/org/dwcj/component/colorchooser/event/ColorChooserChangeEvent.java
@@ -1,11 +1,13 @@
 package org.dwcj.component.colorchooser.event;
 
+import java.awt.Color;
+import java.util.Map;
 import org.dwcj.component.AbstractComponent;
 import org.dwcj.component.event.Event;
 
-import java.awt.*;
-import java.util.Map;
-
+/**
+ * Change Event for the ColorChooser Component.
+ */
 public class ColorChooserChangeEvent extends Event<AbstractComponent> {
   public ColorChooserChangeEvent(AbstractComponent component, Map<String, Object> payload) {
     super(component, payload);
