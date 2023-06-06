963800a2e5c3697fef8325371e5de0c94f2001dd
Oskan <oachmetsik@basis.cloud>
2023-06-06 13:55:53 +0200

    chore: java checkstyle is added for the approveSink

--- dwcj-engine/src/main/java/org/dwcj/component/colorchooser/sink/ColorChooserApproveEventSink.java
+++ dwcj-engine/src/main/java/org/dwcj/component/colorchooser/sink/ColorChooserApproveEventSink.java
@@ -2,23 +2,29 @@ package org.dwcj.component.colorchooser.sink;
 
 import com.basis.bbj.proxies.event.BBjColorChooserApproveEvent;
 import com.basis.bbj.proxies.event.BBjEvent;
-import com.basis.bbj.proxies.sysgui.BBjColorChooser;
 import com.basis.bbj.proxyif.SysGuiEventConstants;
 import com.basis.startup.type.BBjException;
+import java.util.HashMap;
 import org.dwcj.component.AbstractDwcComponent;
 import org.dwcj.component.colorchooser.event.ColorChooserApproveEvent;
 import org.dwcj.component.event.EventDispatcher;
 import org.dwcj.component.event.sink.AbstractSink;
 
-import java.awt.*;
-import java.util.HashMap;
-
+/**
+ * Approve EventSINK for the ColorChooser Component.
+ */
 public class ColorChooserApproveEventSink extends AbstractSink {
 
   public ColorChooserApproveEventSink(AbstractDwcComponent component, EventDispatcher dispatcher) {
     super(component, dispatcher, SysGuiEventConstants.ON_COLORCHOOSER_APPROVE);
   }
 
+  /**
+  * taking care of Events on BBjSide and handing them on DWCj side.
+  *
+  * @param event using it to cast into BBjApproveEvent.
+  * @throws BBjException to handle the event on BBj Side.
+  */
   public void handleEvent(BBjEvent event) throws BBjException {
     BBjColorChooserApproveEvent ev = (BBjColorChooserApproveEvent) event;
     HashMap<String, Object> map = new HashMap<>();
@@ -28,5 +34,4 @@ public class ColorChooserApproveEventSink extends AbstractSink {
     ColorChooserApproveEvent dwcEv = new ColorChooserApproveEvent(component, map);
     this.dispatcher.dispatchEvent(dwcEv);
   }
-
 }
