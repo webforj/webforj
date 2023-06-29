74c15fbc9ecb47d3c1cc387be84f4458e9f6f115
Hyyan Abo Fakher <hyyanaf@gmail.com>
2023-06-29 15:17:23 +0200

    feat: introduce reusable `Expanse` Enum to be reused by components (#378)

---
+++ dwcj-engine/src/main/java/org/dwcj/component/HasExpanse.java
@@ -0,0 +1,28 @@
+package org.dwcj.component;
+
+/**
+ * Interface facilitates implementation of behaviors to modify a component's expanse.
+ *
+ * @param <T> the type of the component
+ * @param <V> the type of the expanse
+ *
+ * @author Hyyan Abo Fakher
+ * @since 23.02
+ */
+public interface HasExpanse<T extends Component, V extends Enum<V> & ExpanseBase> {
+
+  /**
+   * Set the expanse of the component.
+   *
+   * @param expanse the expanse to set for the component
+   * @return the component itself
+   */
+  public T setExpanse(V expanse);
+
+  /**
+   * Returns the expanse of the component.
+   *
+   * @return the expanse of the component
+   */
+  public V getExpanse();
+}
