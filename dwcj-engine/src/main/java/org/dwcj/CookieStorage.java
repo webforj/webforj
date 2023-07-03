d1a9df1f02034abd17d44a827f7159e01024874a
Timon Geisbauer <47525540+TimonGeisbauer@users.noreply.github.com>
2023-07-03 11:23:31 +0200

    feat: implemented a Cookie,Session and LocalStorage class

---
+++ dwcj-engine/src/main/java/org/dwcj/CookieStorage.java
@@ -0,0 +1,51 @@
+package org.dwcj;
+
+import com.basis.bbj.proxies.BBjThinClient;
+import com.basis.startup.type.BBjException;
+import org.dwcj.environment.ObjectTable;
+import org.dwcj.environment.StringTable;
+import org.dwcj.exceptions.DwcjRuntimeException;
+
+/**
+ * Gives access to the browsers cookie storage and allows adding/reading/removing values from it.
+ */
+public class CookieStorage extends AbstractWebStorage {
+
+  private CookieStorage() throws BBjException {
+    super(Environment.getInstance().getBBjAPI().getThinClient(), WebStorageType.COOKIES);
+  }
+
+  CookieStorage(BBjThinClient thinClient) {
+    super(thinClient, WebStorageType.COOKIES);
+  }
+
+  /**
+   * Get the current CookieStorage instance.
+   *
+   * @return the current CookieStorage instance
+   */
+  public static CookieStorage getCurrent() {
+    final String key = "dwcj.cookieStorage.instance";
+
+    if (ObjectTable.contains(key)) {
+      return (CookieStorage) ObjectTable.get(key);
+    }
+
+    try {
+      final CookieStorage instance = new CookieStorage();
+      ObjectTable.put(key, instance);
+      return instance;
+    } catch (BBjException e) {
+      throw new DwcjRuntimeException("Failed to instantiate Cookiestorage.", e);
+    }
+  }
+
+  /**
+   * Sets the cookie path, defaults to window.location.pathname.
+   *
+   * @param path the path the cookies will be stored.
+   */
+  public void setCookiePath(String path) {
+    StringTable.put("!COOKIE_PATH", path);
+  }
+}
---
+++ dwcj-engine/src/main/java/org/dwcj/SessionStorage.java
@@ -0,0 +1,41 @@
+package org.dwcj;
+
+import com.basis.bbj.proxies.BBjThinClient;
+import com.basis.startup.type.BBjException;
+import org.dwcj.environment.ObjectTable;
+import org.dwcj.exceptions.DwcjRuntimeException;
+
+/**
+ * Gives access to the browsers session storage and allows adding/reading/removing values from it.
+ */
+public class SessionStorage extends AbstractWebStorage {
+
+  private SessionStorage() throws BBjException {
+    super(Environment.getInstance().getBBjAPI().getThinClient(), WebStorageType.SESSION);
+  }
+
+  public SessionStorage(BBjThinClient thinClient) {
+    super(thinClient, WebStorageType.SESSION);
+  }
+
+  /**
+   * Get the current SessionStorage instance.
+   *
+   * @return the current SessionStorage instance
+   */
+  public static SessionStorage getCurrent() {
+    final String key = "dwcj.sessionStorage.instance";
+
+    if (ObjectTable.contains(key)) {
+      return (SessionStorage) ObjectTable.get(key);
+    }
+
+    try {
+      final SessionStorage instance = new SessionStorage();
+      ObjectTable.put(key, instance);
+      return instance;
+    } catch (BBjException e) {
+      throw new DwcjRuntimeException("Failed to instantiate SessionStorage.", e);
+    }
+  }
+}
