37ba71455ca726746999ec37dc9b3b1330b46792
Timon Geisbauer <47525540+TimonGeisbauer@users.noreply.github.com>
2023-07-03 13:42:53 +0200

    feat: implemented Cookie,Session and LocalStorage

---
+++ dwcj-engine/src/main/java/org/dwcj/LocalStorage.java
@@ -0,0 +1,41 @@
+package org.dwcj;
+
+import com.basis.bbj.proxies.BBjThinClient;
+import com.basis.startup.type.BBjException;
+import org.dwcj.environment.ObjectTable;
+import org.dwcj.exceptions.DwcjRuntimeException;
+
+/**
+ * Gives access to the browsers local storage and allows adding/reading/removing values from it.
+ */
+public class LocalStorage extends AbstractWebStorage {
+
+  private LocalStorage() throws BBjException {
+    super(Environment.getCurrent().getBBjAPI().getThinClient(), WebStorageType.STORAGE);
+  }
+
+  public LocalStorage(BBjThinClient thinClient) {
+    super(thinClient, WebStorageType.STORAGE);
+  }
+
+  /**
+   * Get the current LocalStorage instance.
+   *
+   * @return the current LocalStorage instance
+   */
+  public static LocalStorage getCurrent() {
+    final String key = "dwcj.localStorage.instance";
+
+    if (ObjectTable.contains(key)) {
+      return (LocalStorage) ObjectTable.get(key);
+    }
+
+    try {
+      final LocalStorage instance = new LocalStorage();
+      ObjectTable.put(key, instance);
+      return instance;
+    } catch (BBjException e) {
+      throw new DwcjRuntimeException("Failed to instantiate LocalStorage.", e);
+    }
+  }
+}
