6788ce290c0d71ad513f8c6bae74a0a7e8cf5970
mhawkinsbasis <113123911+mhawkinsbasis@users.noreply.github.com>
2023-07-04 15:54:09 +0200

    docs: updated some wording in the Javadoc

--- dwcj-engine/src/main/java/org/dwcj/component/HasMax.java
+++ dwcj-engine/src/main/java/org/dwcj/component/HasMax.java
@@ -1,7 +1,7 @@
 package org.dwcj.component;
 
 /**
- * Interface facilitates implementation of behaviors to modify a component's value maximum.
+ * Interface facilitates implementation of behaviors to modify a component's maximum value.
  *
  * @param <T> the type of the component
  * @param <V> the type of the maximum value
@@ -12,7 +12,7 @@ package org.dwcj.component;
 public interface HasMax<T extends Component, V> {
 
   /**
-   * Set the maximum value for the component's value.
+   * Set the maximum possible value for the component.
    *
    * @param max the maximum value
    *
@@ -22,9 +22,9 @@ public interface HasMax<T extends Component, V> {
   public T setMax(V max);
 
   /**
-   * Returns the maximum value for the component's value.
+   * Returns the maximum possible value for the component.
    *
-   * @return the maximum value for the component's value
+   * @return the maximum component value
    * @since 23.02
    */
   public V getMax();
