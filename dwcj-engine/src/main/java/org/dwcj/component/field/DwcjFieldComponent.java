812896c82427f8c6d9bee623eb4dd26a3d9773f1
Hyyan Abo Fakher <hyyanaf@gmail.com>
2023-07-11 11:19:35 +0200

    fix: `DwcjFieldComponent` should extend the `Component` interface

--- dwcj-engine/src/main/java/org/dwcj/component/field/DwcjFieldComponent.java
+++ dwcj-engine/src/main/java/org/dwcj/component/field/DwcjFieldComponent.java
@@ -1,11 +1,13 @@
 package org.dwcj.component.field;
 
+import org.dwcj.component.Component;
+
 /**
  * An interface to mark a DWC field component.
  *
  * @author Hyyan Abo Fakher
  * @since 23.02
  */
-public interface DwcjFieldComponent {
+public interface DwcjFieldComponent extends Component{
 
 }
