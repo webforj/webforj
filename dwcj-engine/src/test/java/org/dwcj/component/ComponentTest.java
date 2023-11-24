6652ed67889d7a555613d6fb3eb7ad279e240aca
Hyyan Abo Fakher <hyyanaf@gmail.com>
2023-11-24 16:00:32 +0100

    style: 'org.dwcj.PendingResult' should be separated from previous import group by one line

--- dwcj-engine/src/test/java/org/dwcj/component/ComponentTest.java
+++ dwcj-engine/src/test/java/org/dwcj/component/ComponentTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
+
 import org.dwcj.PendingResult;
 import org.dwcj.component.ComponentLifecycleObserver.LifecycleEvent;
 import org.dwcj.component.window.Window;
