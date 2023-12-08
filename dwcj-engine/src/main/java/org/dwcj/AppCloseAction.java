34e44a9b9608bbf4238664d514c1486cb70773f6
Hyyan Abo Fakher <hyyanaf@gmail.com>
2023-12-08 07:18:54 +0100

    docs: fixed a typo

--- dwcj-engine/src/main/java/org/dwcj/AppCloseAction.java
+++ dwcj-engine/src/main/java/org/dwcj/AppCloseAction.java
@@ -4,7 +4,7 @@ package org.dwcj;
  * Provides an interface to configure the termination and Error actions of an application.
  *
  * @author Hyyan Abo Fakher
- * @since 23.06Ï
+ * @since 23.06
  */
 public sealed interface AppCloseAction
     permits DefaultAction, NoneAction, MessageAction, RedirectAction {
