cec8e713b7b7399eb29140f99293ab951f049c92
Stephan Wald <swald@basis.cloud>
2024-06-10 00:16:09 -0600

    fix: function order had to be changed. (#606)
    * fix: function order had to be changed.
    * fix: remove DWCJ prompt
    
    ---------
    
    Co-authored-by: Stephan Wald <swald@basis.com>

--- webforj-components/webforj-terminal/src/main/java/com/webforj/component/terminal/Terminal.java
+++ webforj-components/webforj-terminal/src/main/java/com/webforj/component/terminal/Terminal.java
@@ -30,28 +30,26 @@ public class Terminal extends Panel {
     add(hv);
 
 
-    String script = "function send(event){var custom=new CustomEvent("
+    String script = "function whenTerminalLoaded (callback) { if (typeof Terminal === 'undefined' ) "
+      + "{setTimeout (function () {whenTerminalLoaded (callback);}, 100);} "
+      + "else { callback (); }}";
+    script += "function send(event){var custom=new CustomEvent("
         + "'custom_event',{bubbles:true,cancelable:true});" + "custom.key=event.key;"
         + "window.basisDispatchCustomEvent(event.target,custom);}";
     hv.injectScript(script, true);
 
-    script = "function whenTerminalLoaded (callback) { if (typeof Terminal === 'undefined' ) "
-        + "{setTimeout (function () {whenTerminalLoaded (callback);}, 100);} "
-        + "else { callback (); }}";
-    hv.injectScript(script);
-
     script = "var link =  $doc.createElement('script');"
         + "link.setAttribute('type','text/javascript');"
-        + "link.setAttribute('src','https://cdn.jsdelivr.net/npm/xterm@4.13.0/lib/xterm.js');"
+        + "link.setAttribute('src','https://cdn.jsdelivr.net/npm/xterm@5.3.0/lib/xterm.js');"
         + "document.head.appendChild(link);" + "var link2 =  $doc.createElement('link');"
         + "link2.setAttribute('rel','stylesheet');"
-        + "link2.setAttribute('href','https://cdn.jsdelivr.net/npm/xterm@4.13.0/css/xterm.css');"
+        + "link2.setAttribute('href','https://cdn.jsdelivr.net/npm/xterm@5.3.0/css/xterm.css');"
         + "document.head.appendChild(link2);";
     hv.executeScript(script);
 
     script = "whenTerminalLoaded(function() {window.term = new window.Terminal();"
         + "window.term.open(document.getElementById('" + uuid + "'));"
-        + "window.term.write('\\x1B[1;3;31mDWCJ\\x1B[0m Terminal');" + "window.term.focus();"
+        + "window.term.focus();"
         + "window.term.onKey(" + "function ({key, domEvent}) " + "{" + "send(domEvent);" + "})})";
     hv.executeScript(script);
 
