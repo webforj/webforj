4f5744435ea80ac7ce2380e87c822945b5237363
Hyyan Abo Fakher <hyyanaf@gmail.com>
2024-07-01 15:53:02 +0200

    test: add constructor tests

--- webforj-components/webforj-toast/src/test/java/com/webforj/component/toast/ToastTest.java
+++ webforj-components/webforj-toast/src/test/java/com/webforj/component/toast/ToastTest.java
@@ -2,12 +2,20 @@ package com.webforj.component.toast;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
 
+import com.webforj.App;
+import com.webforj.component.Theme;
 import com.webforj.component.element.PropertyDescriptorTester;
+import com.webforj.component.window.Frame;
+import java.util.Collections;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 class ToastTest {
 
@@ -16,6 +24,115 @@ class ToastTest {
   @BeforeEach
   void setUp() {
     component = new Toast();
+  }
+
+  @Nested
+  class Constructors {
+    Frame mockFrame;
+
+    @BeforeEach
+    public void setUp() {
+      mockFrame = mock(Frame.class);
+    }
+
+    @Test
+    void shouldCreateWithTextDurationThemeAndPlacement() {
+      try (MockedStatic<App> mockedApp = mockStatic(App.class)) {
+        when(App.getFrames()).thenReturn(Collections.singletonList(mockFrame));
+
+        component = Toast.show("Hello, World!", 5000, Theme.INFO, Toast.Placement.TOP_RIGHT);
+        assertEquals("Hello, World!", component.getText());
+        assertEquals(5000, component.getDuration());
+        assertEquals(Theme.INFO, component.getTheme());
+        assertEquals(Toast.Placement.TOP_RIGHT, component.getPlacement());
+      }
+    }
+
+    @Test
+    void shouldCreateWithTextDurationAndTheme() {
+      try (MockedStatic<App> mockedApp = mockStatic(App.class)) {
+        when(App.getFrames()).thenReturn(Collections.singletonList(mockFrame));
+        component = Toast.show("Hello, World!", 5000, Theme.INFO);
+
+        assertEquals("Hello, World!", component.getText());
+        assertEquals(5000, component.getDuration());
+        assertEquals(Theme.INFO, component.getTheme());
+        assertEquals(Toast.Placement.BOTTOM, component.getPlacement());
+      }
+    }
+
+    @Test
+    void shouldCreateWithTextAndTheme() {
+      try (MockedStatic<App> mockedApp = mockStatic(App.class)) {
+        when(App.getFrames()).thenReturn(Collections.singletonList(mockFrame));
+        component = Toast.show("Hello, World!", Theme.INFO);
+
+        assertEquals("Hello, World!", component.getText());
+        assertEquals(Toast.DEFAULT_DURATION, component.getDuration());
+        assertEquals(Theme.INFO, component.getTheme());
+        assertEquals(Toast.Placement.BOTTOM, component.getPlacement());
+      }
+    }
+
+    @Test
+    void shouldCreateWithTextDurationAndPlacement() {
+      try (MockedStatic<App> mockedApp = mockStatic(App.class)) {
+        when(App.getFrames()).thenReturn(Collections.singletonList(mockFrame));
+        component = Toast.show("Hello, World!", 5000, Toast.Placement.TOP_RIGHT);
+
+        assertEquals("Hello, World!", component.getText());
+        assertEquals(5000, component.getDuration());
+        assertEquals(Theme.DEFAULT, component.getTheme());
+        assertEquals(Toast.Placement.TOP_RIGHT, component.getPlacement());
+      }
+    }
+
+    @Test
+    void shouldCreateWithTextAndPlacement() {
+      try (MockedStatic<App> mockedApp = mockStatic(App.class)) {
+        when(App.getFrames()).thenReturn(Collections.singletonList(mockFrame));
+        component = Toast.show("Hello, World!", Toast.Placement.TOP_RIGHT);
+
+        assertEquals("Hello, World!", component.getText());
+        assertEquals(Toast.DEFAULT_DURATION, component.getDuration());
+        assertEquals(Theme.DEFAULT, component.getTheme());
+        assertEquals(Toast.Placement.TOP_RIGHT, component.getPlacement());
+      }
+    }
+
+    @Test
+    void shouldCreateWithTextAndDuration() {
+      try (MockedStatic<App> mockedApp = mockStatic(App.class)) {
+        when(App.getFrames()).thenReturn(Collections.singletonList(mockFrame));
+        component = Toast.show("Hello, World!", 5000);
+
+        assertEquals("Hello, World!", component.getText());
+        assertEquals(5000, component.getDuration());
+        assertEquals(Theme.DEFAULT, component.getTheme());
+        assertEquals(Toast.Placement.BOTTOM, component.getPlacement());
+      }
+    }
+
+    @Test
+    void shouldCreateWithText() {
+      try (MockedStatic<App> mockedApp = mockStatic(App.class)) {
+        when(App.getFrames()).thenReturn(Collections.singletonList(mockFrame));
+        component = Toast.show("Hello, World!");
+
+        assertEquals("Hello, World!", component.getText());
+        assertEquals(Toast.DEFAULT_DURATION, component.getDuration());
+        assertEquals(Theme.DEFAULT, component.getTheme());
+        assertEquals(Toast.Placement.BOTTOM, component.getPlacement());
+      }
+    }
+
+    @Test
+    void shouldCreateWithDefaultValues() {
+      assertEquals("", component.getText());
+      assertEquals(Toast.DEFAULT_DURATION, component.getDuration());
+      assertEquals(Theme.DEFAULT, component.getTheme());
+      assertEquals(Toast.Placement.BOTTOM, component.getPlacement());
+    }
   }
 
   @Nested
