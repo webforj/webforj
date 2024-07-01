875a6af71693b41ce8e3ba32393e6e4f49970590
Hyyan Abo Fakher <hyyanaf@gmail.com>
2024-07-01 14:29:00 +0200

    feat: add `Button` constructors with theme parameter

--- webforj-foundation/src/test/java/com/webforj/component/button/ButtonTest.java
+++ webforj-foundation/src/test/java/com/webforj/component/button/ButtonTest.java
@@ -3,31 +3,48 @@ package com.webforj.component.button;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.webforj.component.button.event.ButtonClickEvent;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ButtonTest {
 
-  @Test
-  @DisplayName("Constructor with text and click listener")
-  void constructorWithTextAndClickListener() {
-    Button button = new Button("Click Me", e -> {
-    });
-    assertEquals("Click Me", button.getText());
-    assertEquals(1, button.getEventListeners(ButtonClickEvent.class).size());
-  }
+  @Nested
+  class Constructors {
 
-  @Test
-  @DisplayName("Constructor with text only")
-  void constructorWithTextOnly() {
-    Button button = new Button("Click Me");
-    assertEquals("Click Me", button.getText());
-  }
+    @Test
+    void shouldCreateWithTextThemeAndClickListener() {
+      Button button = new Button("Click Me", ButtonTheme.DEFAULT, e -> {
+      });
+      assertEquals("Click Me", button.getText());
+      assertEquals(ButtonTheme.DEFAULT, button.getTheme());
+      assertEquals(1, button.getEventListeners(ButtonClickEvent.class).size());
+    }
 
-  @Test
-  @DisplayName("Constructor with no arguments")
-  void constructorWithNoArguments() {
-    Button button = new Button();
-    assertEquals("", button.getText());
+    @Test
+    void shouldCreateWithTextAndTheme() {
+      Button button = new Button("Click Me", ButtonTheme.DEFAULT);
+      assertEquals("Click Me", button.getText());
+      assertEquals(ButtonTheme.DEFAULT, button.getTheme());
+    }
+
+    @Test
+    void shouldCreateWithTextAndClickListener() {
+      Button button = new Button("Click Me", e -> {
+      });
+      assertEquals("Click Me", button.getText());
+      assertEquals(1, button.getEventListeners(ButtonClickEvent.class).size());
+    }
+
+    @Test
+    void shouldCreateWithTextOnly() {
+      Button button = new Button("Click Me");
+      assertEquals("Click Me", button.getText());
+    }
+
+    @Test
+    void shouldCreateWithNoArguments() {
+      Button button = new Button();
+      assertEquals("", button.getText());
+    }
   }
 }
