03ddac4b1d495d9f4980c26c14e4de1e48e127b5
Hyyan Abo Fakher <hyyanaf@gmail.com>
2024-07-16 15:17:33 +0200

    feat: add webforj-terminal component

--- webforj-components/webforj-terminal/src/main/java/com/webforj/component/terminal/events/TerminalKeyEvent.java
+++ webforj-components/webforj-terminal/src/main/java/com/webforj/component/terminal/events/TerminalKeyEvent.java
@@ -1,57 +1,102 @@
 package com.webforj.component.terminal.events;
 
-import com.webforj.component.Component;
-import com.webforj.component.ControlEvent;
+import com.webforj.component.element.annotation.EventName;
+import com.webforj.component.element.annotation.EventOptions;
+import com.webforj.component.element.annotation.EventOptions.EventData;
+import com.webforj.component.event.ComponentEvent;
 import com.webforj.component.terminal.Terminal;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Map;
 
-public final class TerminalKeyEvent implements ControlEvent, TerminalKey {
+/**
+ * Fired when a key is pressed.
+ *
+ * @author Hyyan Abo Fakher
+ * @since 24.10
+ */
+@EventName(value = "dwc-key")
+@EventOptions(filter = "event.target.isSameNode(component)",
+    data = {@EventData(key = "code", exp = "event.detail.event.code"),
+        @EventData(key = "key", exp = "event.detail.event.key"),
+        @EventData(key = "value", exp = "btoa(event.detail.value)"),
+        @EventData(key = "altKey", exp = "event.detail.event.altKey"),
+        @EventData(key = "controlKey", exp = "event.detail.event.ctrlKey"),
+        @EventData(key = "shiftKey", exp = "event.detail.event.shiftKey")})
+public final class TerminalKeyEvent extends ComponentEvent<Terminal> {
 
-  private final Terminal ctrl;
-  private final Map<String, String> eventMap;
-
-  public TerminalKeyEvent(Terminal t, Map<String, String> getEventMap) {
-    this.ctrl = t;
-    this.eventMap = getEventMap;
+  /**
+   * Creates a new TerminalKeyEvent.
+   *
+   * @param component the component
+   * @param detail the detail of the event
+   */
+  public TerminalKeyEvent(Terminal component, Map<String, Object> detail) {
+    super(component, detail);
   }
 
-  @Override
-  public Component getControl() {
-    return ctrl;
-  }
-
-  @Override
-  public boolean getAltDown() {
-    return false;
-  }
-
-  @Override
-  public boolean getCtrlDown() {
-    return false;
-  }
-
-  @Override
-  public boolean getShiftDown() {
-    return false;
-  }
-
-  @Override
-  public boolean getMetaDown() {
-    return false;
-  }
-
-  @Override
-  public String getKey() {
-    return (String) eventMap.get("key");
-  }
-
-  @Override
+  /**
+   * Returns the code for the key that was pressed as defined in the browser.
+   *
+   * @return the code for the key that was pressed
+   */
   public String getCode() {
-    return (String) eventMap.get("keyCode");
+    return (String) this.getEventMap().get("code");
   }
 
+  /**
+   * Returns the key that was pressed.
+   *
+   * @return the key that was pressed
+   */
+  public String getKey() {
+    return (String) this.getEventMap().get("key");
+  }
+
+  /**
+   * Returns the value of the key that was pressed.
+   *
+   * @return the value of the key that was pressed
+   */
+  public String getValue() {
+    String value = (String) this.getEventMap().get("value");
+    return new String(Base64.getDecoder().decode(value), StandardCharsets.UTF_8);
+  }
+
+  /**
+   * Returns whether or not the alt key was pressed when the event happened.
+   *
+   * @return A boolean representing whether alt was pressed.
+   */
+  public boolean isAltKey() {
+    return (boolean) this.getEventMap().get("altKey");
+  }
+
+  /**
+   * Returns whether or not the control key was pressed when the event happened.
+   *
+   * @return A boolean representing whether ctrl was pressed.
+   */
+  public boolean isControlKey() {
+    return (boolean) this.getEventMap().get("controlKey");
+  }
+
+  /**
+   * Returns whether or not the shift key was pressed when the event happened.
+   *
+   * @return A boolean representing whether shift was pressed.
+   */
+  public boolean isShiftKey() {
+    return (boolean) this.getEventMap().get("shiftKey");
+  }
+
+  /**
+   * Returns the terminal component.
+   *
+   * @return the terminal component
+   */
   @Override
-  public int getKeyCode() {
-    return Integer.parseInt(eventMap.get("keyCode"));
+  public Terminal getComponent() {
+    return (Terminal) super.getComponent();
   }
 }
---
+++ webforj-components/webforj-terminal/src/test/java/com/webforj/component/terminal/TerminalTest.java
@@ -0,0 +1,241 @@
+package com.webforj.component.terminal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.webforj.PendingResult;
+import com.webforj.component.element.Element;
+import com.webforj.component.element.PropertyDescriptorTester;
+import com.webforj.component.terminal.events.TerminalDataEvent;
+import com.webforj.component.terminal.events.TerminalKeyEvent;
+import com.webforj.dispatcher.EventListener;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class TerminalTest {
+
+  Terminal component;
+
+  @BeforeEach
+  void setUp() {
+    component = new Terminal();
+  }
+
+  @Nested
+  @DisplayName("Properties API")
+  class PropertiesApi {
+
+    @Test
+    void shouldSetGetProperties() {
+      try {
+        PropertyDescriptorTester.run(Terminal.class, component);
+      } catch (Exception e) {
+        fail("PropertyDescriptor test failed: " + e.getMessage());
+      }
+    }
+  }
+
+  @Nested
+  @DisplayName("Methods API")
+  class MethodsApi {
+
+    @Test
+    void shouldFocus() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.focus();
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_SET_FOCUS);
+    }
+
+    @Test
+    void shouldBlur() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.blur();
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_REMOVE_FOCUS);
+    }
+
+    @Test
+    void shouldWrite() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.write("Hello");
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_WRITE, "Hello");
+    }
+
+    @Test
+    void shouldWriteln() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.writeln("Hello");
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_WRITELN, "Hello");
+    }
+
+    @Test
+    void shouldClear() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.clear();
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_CLEAR);
+    }
+
+    @Test
+    void shouldReset() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.reset();
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_RESET);
+    }
+
+    @Test
+    void shouldFit() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.fit();
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_FIT);
+    }
+
+    @Test
+    void shouldSelectAll() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.selectAll();
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_SELECT_ALL);
+    }
+
+    @Test
+    void shouldSelect() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.select(1, 2, 3);
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_SELECT, 1, 2, 3);
+    }
+
+    @Test
+    void shouldSelectLines() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.selectLines(1, 2);
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_SELECT_LINES, 1, 2);
+    }
+
+    @Test
+    void shouldClearSelection() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.clearSelection();
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_CLEAR_SELECTION);
+    }
+
+    @Test
+    void shouldGetSelection() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+      when(element.callJsFunctionAsync(Terminal.JS_METHOD_GET_SELECTION))
+          .thenReturn(PendingResult.completedWith("Hello"));
+
+      PendingResult<String> selected = mock.getSelectedText();
+      selected.thenAccept(text -> assertEquals("Hello", text));
+    }
+
+    @Test
+    void shouldScrollLines() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.scrollLines(1);
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_SCROLL_LINES, 1);
+    }
+
+    @Test
+    void shouldScrollToLine() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.scrollToLine(1);
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_SCROLL_TO_LINE, 1);
+    }
+
+    @Test
+    void shouldScrollToTop() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.scrollToTop();
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_SCROLL_TO_TOP);
+    }
+
+    @Test
+    void shouldScrollToBottom() {
+      Terminal mock = spy(Terminal.class);
+      Element element = mock(Element.class);
+      when(mock.getOriginalElement()).thenReturn(element);
+
+      mock.scrollToBottom();
+      verify(element).callJsFunctionAsync(Terminal.JS_METHOD_SCROLL_TO_BOTTOM);
+    }
+  }
+
+  @Nested
+  @DisplayName("Events API")
+  class EventsApi {
+
+    @Test
+    void shouldAddDataListener() {
+      component.onData(event -> {
+      });
+
+      List<EventListener<TerminalDataEvent>> listeners =
+          component.getEventListeners(TerminalDataEvent.class);
+
+      assertEquals(1, listeners.size());
+      assertTrue(listeners.get(0) instanceof EventListener<TerminalDataEvent>);
+    }
+
+    @Test
+    void shouldAddKeyListener() {
+      component.onKey(event -> {
+      });
+
+      List<EventListener<TerminalKeyEvent>> listeners =
+          component.getEventListeners(TerminalKeyEvent.class);
+
+      assertEquals(1, listeners.size());
+      assertTrue(listeners.get(0) instanceof EventListener<TerminalKeyEvent>);
+    }
+  }
+}
