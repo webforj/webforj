4ad3c3618aaeb36970e937d4562db22ebcc6e79e
Hyyan Abo Fakher <hyyanaf@gmail.com>
2024-09-26 09:04:21 +0200

    refactor: remove unused import in MemoryHistoryTest

--- webforj-foundation/src/test/java/com/webforj/router/history/MemoryHistoryTest.java
+++ webforj-foundation/src/test/java/com/webforj/router/history/MemoryHistoryTest.java
@@ -2,7 +2,6 @@ package com.webforj.router.history;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
