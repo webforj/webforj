f039a0d7b44682e80bb640140220720114693c4a
Hyyan Abo Fakher <hyyanaf@gmail.com>
2025-01-20 11:12:53 +0100

    feat: add support for configuring Icon expansion (#875)

---
+++ webforj-components/webforj-icons/src/main/java/com/webforj/component/icons/IconExpanse.java
@@ -0,0 +1,53 @@
+package com.webforj.component.icons;
+
+
+import com.google.gson.annotations.SerializedName;
+import com.webforj.component.ExpanseBase;
+
+/**
+ * Supported expanses as defined by the Dwc Theme Engine.
+ *
+ * @author Hyyan Abo Fakher
+ * @since 24.21
+ */
+public enum IconExpanse implements ExpanseBase {
+  /* The 3xlarge expanse as defined by the Dwc Theme Engine. */
+  @SerializedName("3xl")
+  XXXLARGE,
+
+  /* The 2xlarge expanse as defined by the Dwc Theme Engine. */
+  @SerializedName("2xl")
+  XXLARGE,
+
+  /* The xlarge expanse as defined by the Dwc Theme Engine. */
+  @SerializedName("xl")
+  XLARGE,
+
+  /* The large expanse as defined by the Dwc Theme Engine. */
+  @SerializedName("l")
+  LARGE,
+
+  /* The medium expanse as defined by the Dwc Theme Engine. */
+  @SerializedName("m")
+  MEDIUM,
+
+  /* The small expanse as defined by the Dwc Theme Engine. */
+  @SerializedName("s")
+  SMALL,
+
+  /* The xsmall expanse as defined by the Dwc Theme Engine. */
+  @SerializedName("xs")
+  XSMALL,
+
+  /* The 2xsmall expanse as defined by the Dwc Theme Engine. */
+  @SerializedName("2xs")
+  XXSMALL,
+
+  /* The 3xsmall expanse as defined by the Dwc Theme Engine. */
+  @SerializedName("3xs")
+  XXXSMALL,
+
+  /* No expanse is applied. */
+  @SerializedName("")
+  NONE;
+}
