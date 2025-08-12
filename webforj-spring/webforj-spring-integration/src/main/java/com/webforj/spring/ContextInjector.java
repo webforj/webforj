9e2531445a6c4cf82062548a76fb9248f2ae3fa2
Hyyan Abo Fakher <hyyanaf@gmail.com>
2025-08-12 12:39:18 +0200

    fix: `ContextHolder` does not free the `ApplicationContext` when `ContextInjector` is disposed (#1073)

--- webforj-spring/webforj-spring-integration/src/main/java/com/webforj/spring/ContextInjector.java
+++ webforj-spring/webforj-spring-integration/src/main/java/com/webforj/spring/ContextInjector.java
@@ -2,6 +2,7 @@ package com.webforj.spring;
 
 import java.lang.System.Logger;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
@@ -13,12 +14,18 @@ import org.springframework.stereotype.Component;
  * @since 25.02
  */
 @Component
-public class ContextInjector implements ApplicationContextAware {
+public class ContextInjector implements ApplicationContextAware, DisposableBean {
   Logger logger = System.getLogger(ContextInjector.class.getName());
 
   @Override
   public void setApplicationContext(ApplicationContext context) throws BeansException {
     ContextHolder.setContext(context);
     logger.log(Logger.Level.DEBUG, "Injected ApplicationContext into ContextHolder.");
+  }
+
+  @Override
+  public void destroy() throws Exception {
+    ContextHolder.clear();
+    logger.log(Logger.Level.DEBUG, "Cleared ApplicationContext from ContextHolder.");
   }
 }
