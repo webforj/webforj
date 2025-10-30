d72521aaa4392138ad824a77bf1b33dddbdb8005
Kevin Hagel <khagel@basis.cloud>
2025-10-30 10:05:50 +0200

    fix: remove unused assertNotNull import in MinifyMojoIntegrationTest
    Removed unused static import to resolve SonarQube code quality violation.

--- webforj-plugins/webforj-minify/webforj-minify-maven-plugin/src/test/java/com/webforj/minify/maven/MinifyMojoIntegrationTest.java
+++ webforj-plugins/webforj-minify/webforj-minify-maven-plugin/src/test/java/com/webforj/minify/maven/MinifyMojoIntegrationTest.java
@@ -2,7 +2,6 @@ package com.webforj.minify.maven;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
