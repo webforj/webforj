94f495bc0eb7a992c3fc8dcd12bd9e4151bc7eb0
cliebhardt <liebhardt.christian@proton.me>
2026-01-28 12:16:16 +0100

    fix!: made repository mandatory for navigator DSL method

--- webforj-kotlin/src/main/kotlin/com/webforj/kotlin/dsl/component/navigator/Navigator.kt
+++ webforj-kotlin/src/main/kotlin/com/webforj/kotlin/dsl/component/navigator/Navigator.kt
@@ -59,7 +59,7 @@ fun @WebforjDsl HasComponents.navigator(
 }
 
 /**
- * Creates a `Navigator` with optional [text], [layout], [pageSize] and / or [repository].
+ * Creates a `Navigator` with optional [text], [layout], [pageSize] and / or a mandatory [repository].
  * ```
  * ... {
  *   navigator() // Empty Navigator component
@@ -78,31 +78,23 @@ fun @WebforjDsl HasComponents.navigator(
  * @see [paginator]
  */
 fun @WebforjDsl HasComponents.navigator(
+  repository: Repository<*>,
   text: String? = null,
   layout: Navigator.Layout? = null,
   pageSize: Int? = null,
-  repository: Repository<*>? = null,
   block: @WebforjDsl Navigator.() -> Unit = {}
 ): Navigator {
   val navigator = when {
-    text != null -> Navigator(text).apply {
-      pageSize?.let { paginator.size = it }
-      layout?.let { setLayout(it) }
-    }
-    layout != null && pageSize != null && repository != null ->
-      Navigator(repository, pageSize, layout).apply {
+    layout != null && pageSize != null -> Navigator(repository, pageSize, layout).apply {
         text?.let { setText(it) }
       }
-    layout != null && repository != null -> Navigator(repository, layout).apply {
+    layout != null -> Navigator(repository, layout).apply {
       text?.let { setText(it) }
     }
-    pageSize != null && repository != null -> Navigator(repository, pageSize).apply {
+    pageSize != null -> Navigator(repository, pageSize).apply {
       text?.let { setText(it) }
     }
-    repository != null -> Navigator(repository).apply {
-      text?.let { setText(it) }
-    }
-    else -> Navigator().apply {
+    else -> Navigator(repository).apply {
       pageSize?.let { paginator.size = it }
       layout?.let { setLayout(it) }
     }
