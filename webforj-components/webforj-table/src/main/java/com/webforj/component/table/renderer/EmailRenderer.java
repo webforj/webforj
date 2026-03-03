5857c12a2ea6f6cdfce03b5c0cf887fc120522db
Hyyan Abo Fakher <hyyanaf@gmail.com>
2026-03-03 08:40:26 +0100

    refactor: implement AbstractActionLinkRenderer and refactor EmailRenderer and PhoneRenderer to extend it

--- webforj-components/webforj-table/src/main/java/com/webforj/component/table/renderer/EmailRenderer.java
+++ webforj-components/webforj-table/src/main/java/com/webforj/component/table/renderer/EmailRenderer.java
@@ -1,8 +1,5 @@
 package com.webforj.component.table.renderer;
 
-import com.webforj.component.Theme;
-import com.webforj.component.element.annotation.NodeName;
-import com.webforj.component.icons.Icon;
 import com.webforj.component.icons.IconDefinition;
 import com.webforj.component.icons.TablerIcon;
 
@@ -23,10 +20,7 @@ import com.webforj.component.icons.TablerIcon;
  * @author Hyyan Abo Fakher
  * @since 25.12
  */
-@NodeName("a")
-public class EmailRenderer<T> extends AbstractElementRenderer<T> {
-  private IconDefinition<?> icon = TablerIcon.create("mail");
-  private Theme iconTheme = Theme.PRIMARY;
+public class EmailRenderer<T> extends AbstractActionLinkRenderer<T> {
 
   /**
    * Creates a new email renderer with the given icon.
@@ -34,62 +28,19 @@ public class EmailRenderer<T> extends AbstractElementRenderer<T> {
    * @param icon the icon to display before the email address
    */
   public EmailRenderer(IconDefinition<?> icon) {
-    super(null, null);
-    this.icon = icon;
-    applyIconTheme(icon);
+    super(icon);
   }
 
   /**
    * Creates a new email renderer with the default {@code mail} icon.
    */
   public EmailRenderer() {
-    super(null, null);
-  }
-
-  /**
-   * Sets the icon to display before the email address.
-   *
-   * @param icon the icon definition
-   * @return this renderer
-   */
-  public EmailRenderer<T> setIcon(IconDefinition<?> icon) {
-    this.icon = icon;
-    applyIconTheme(icon);
-    fireChangeEvent();
-    return this;
-  }
-
-  /**
-   * Returns the icon displayed before the email address.
-   *
-   * @return the icon, or {@code null} if not set
-   */
-  public IconDefinition<?> getIcon() {
-    return icon;
+    super(TablerIcon.create("mail"));
   }
 
   /** {@inheritDoc} */
   @Override
-  public String build() {
-    setAttribute("href", "<%= 'mailto:' + cell.value %>", false);
-    setAttribute("tabindex", "-1", false);
-    setAttribute("style", "color:inherit;text-decoration:none", false);
-
-    if (icon != null) {
-      String iconHtml = "<dwc-icon name='" + icon.getName() + "' pool='" + icon.getPool()
-          + "' theme='" + toAttributeValue(iconTheme) + "'></dwc-icon> ";
-      String html = super.build();
-      String content = getContent();
-      String resolvedContent = content == null ? "<%= cell.value %>" : content;
-      return html.replace(resolvedContent, iconHtml + resolvedContent);
-    }
-
-    return super.build();
-  }
-
-  private void applyIconTheme(IconDefinition<?> icon) {
-    if (icon instanceof Icon iconInstance && iconInstance.getTheme() != null) {
-      iconTheme = iconInstance.getTheme();
-    }
+  protected String getHrefPrefix() {
+    return "mailto:";
   }
 }
--- webforj-components/webforj-table/src/main/java/com/webforj/component/table/renderer/PhoneRenderer.java
+++ webforj-components/webforj-table/src/main/java/com/webforj/component/table/renderer/PhoneRenderer.java
@@ -1,8 +1,5 @@
 package com.webforj.component.table.renderer;
 
-import com.webforj.component.Theme;
-import com.webforj.component.element.annotation.NodeName;
-import com.webforj.component.icons.Icon;
 import com.webforj.component.icons.IconDefinition;
 import com.webforj.component.icons.TablerIcon;
 
@@ -23,10 +20,7 @@ import com.webforj.component.icons.TablerIcon;
  * @author Hyyan Abo Fakher
  * @since 25.12
  */
-@NodeName("a")
-public class PhoneRenderer<T> extends AbstractElementRenderer<T> {
-  private IconDefinition<?> icon = TablerIcon.create("phone");
-  private Theme iconTheme = Theme.PRIMARY;
+public class PhoneRenderer<T> extends AbstractActionLinkRenderer<T> {
 
   /**
    * Creates a new phone renderer with the given icon.
@@ -34,62 +28,19 @@ public class PhoneRenderer<T> extends AbstractElementRenderer<T> {
    * @param icon the icon to display before the phone number
    */
   public PhoneRenderer(IconDefinition<?> icon) {
-    super(null, null);
-    this.icon = icon;
-    applyIconTheme(icon);
+    super(icon);
   }
 
   /**
    * Creates a new phone renderer with the default {@code phone} icon.
    */
   public PhoneRenderer() {
-    super(null, null);
-  }
-
-  /**
-   * Sets the icon to display before the phone number.
-   *
-   * @param icon the icon definition
-   * @return this renderer
-   */
-  public PhoneRenderer<T> setIcon(IconDefinition<?> icon) {
-    this.icon = icon;
-    applyIconTheme(icon);
-    fireChangeEvent();
-    return this;
-  }
-
-  /**
-   * Returns the icon displayed before the phone number.
-   *
-   * @return the icon, or {@code null} if not set
-   */
-  public IconDefinition<?> getIcon() {
-    return icon;
+    super(TablerIcon.create("phone"));
   }
 
   /** {@inheritDoc} */
   @Override
-  public String build() {
-    setAttribute("href", "<%= 'tel:' + cell.value %>", false);
-    setAttribute("tabindex", "-1", false);
-    setAttribute("style", "color:inherit;text-decoration:none", false);
-
-    if (icon != null) {
-      String iconHtml = "<dwc-icon name='" + icon.getName() + "' pool='" + icon.getPool()
-          + "' theme='" + toAttributeValue(iconTheme) + "'></dwc-icon> ";
-      String html = super.build();
-      String content = getContent();
-      String resolvedContent = content == null ? "<%= cell.value %>" : content;
-      return html.replace(resolvedContent, iconHtml + resolvedContent);
-    }
-
-    return super.build();
-  }
-
-  private void applyIconTheme(IconDefinition<?> icon) {
-    if (icon instanceof Icon iconInstance && iconInstance.getTheme() != null) {
-      iconTheme = iconInstance.getTheme();
-    }
+  protected String getHrefPrefix() {
+    return "tel:";
   }
 }
